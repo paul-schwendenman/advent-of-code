330e45511ea25ee26a9758a4c63566469e78ca2b
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-02 01:29:52 -0500

    Refactor day 2 solution

--- 2020/day02/day02.py
+++ 2020/day02/day02.py
@@ -1,39 +1,69 @@
 from collections import Counter
+from dataclasses import dataclass
 import fileinput
 import re
 
 
-def parse_line(line):
-    return re.match(r'(?P<min>[0-9]+)-(?P<max>[0-9]+) (?P<char>.): (?P<password>.+)', line).groups()
+@dataclass
+class Password():
+    lower_bound: int
+    upper_bound: int
+    target_char: str
+    password: str
+
+    _pattern = re.compile(r'(?P<lower_bound>[0-9]+)-(?P<upper_bound>[0-9]+) (?P<target_char>.): (?P<password>.+)')
+
+    def __init__(self, lower_bound, upper_bound, target_char, password):
+        self.lower_bound = int(lower_bound)
+        self.upper_bound = int(upper_bound)
+        self.target_char = target_char
+        self.password = password
+
+    @classmethod
+    def make(cls, line):
+        match = cls._pattern.match(line).groupdict()
+
+        return cls(**match)
+
+    @property
+    def lower_char(self):
+        return self.password[self.lower_bound - 1]
+
+    @property
+    def upper_char(self):
+        return self.password[self.upper_bound - 1]
+
+    def counts(self):
+        return Counter(self.password)
+
+
+def is_valid_part1_password(line):
+    password = Password.make(line)
+    count = password.counts()[password.target_char]
+
+    return password.lower_bound <= count and count <= password.upper_bound
+
+
+def is_valid_part2_password(line):
+    password = Password.make(line)
+
+    lower_match = password.lower_char == password.target_char
+    upper_match = password.upper_char == password.target_char
+
+    return lower_match ^ upper_match
 
 
 def part1(lines):
-    count = 0
-    for line in lines:
-        min, max, char, password = parse_line(line)
-
-        c = Counter(password)
-
-        if c[char] >= int(min) and c[char] <= int(max):
-            count += 1
-
-    return count
+    return sum([is_valid_part1_password(line) for line in lines])
 
 
 def part2(lines):
-    count = 0
-    for line in lines:
-        min, max, char, password = parse_line(line)
-
-        if (password[int(min)-1] == char and password[int(max)-1] != char) or (password[int(min)-1] != char and password[int(max)-1] == char):
-            count += 1
-
-    return count
+    return sum([is_valid_part2_password(line) for line in lines])
 
 
 def main():
     with fileinput.input() as data:
-        lines = [line for line in data]
+        lines = list(data)
 
     print(part1(lines))
     print(part2(lines))
