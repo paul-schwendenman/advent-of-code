297013c8b3c0edde9cbd822b1683430f6da2ebe3
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-01 23:39:12 -0500

    Add skeleton for day 2

---
+++ 2020/day02/day02.py
@@ -0,0 +1,21 @@
+import fileinput
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+def main():
+    with fileinput.input() as data:
+        lines = data
+
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2020/day02/test_day02.py
@@ -0,0 +1,9 @@
+from day02 import part1, part2
+
+
+# def test_part1():
+#     assert part1() == 1
+
+
+# def test_part2():
+#     assert part2() == 1
