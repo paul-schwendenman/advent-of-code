d32a870cd0fa2edcadbc3cd86e318f0abcb6346c
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-03 00:45:06 -0500

    Add tests

--- 2020/day03/test_day03.py
+++ 2020/day03/test_day03.py
@@ -1,5 +1,16 @@
+import pytest
 from day03 import part1, part2
 
 
-def test_part1_sample_1():
-    assert part1(['1-3 a: abcde']) == 1
+@pytest.fixture
+def sample_map():
+    with open('sample') as input_file:
+        yield input_file.read().splitlines()
+
+
+def test_part1_sample(sample_map):
+    assert part1(sample_map) == 7
+
+
+def test_part2_sample(sample_map):
+    assert part2(sample_map) == 336
