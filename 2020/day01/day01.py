a795030f15dbcb0a8e6ebd58ab47c485b8994dc2
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-03 23:22:32 -0500

    Add type hints to day 1

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -1,3 +1,4 @@
+from typing import List
 import fileinput
 import functools
 import itertools
@@ -8,7 +9,7 @@ def prod(*args: int) -> int:
     return functools.reduce(operator.mul, args)
 
 
-def calculate_report(expenses, *, entry_count=2, desired_sum=2020):
+def calculate_report(expenses: List[int], *, entry_count: int = 2, desired_sum: int = 2020) -> int:
     pairs = itertools.combinations(expenses, entry_count)
 
     for pairing in pairs:
@@ -18,15 +19,15 @@ def calculate_report(expenses, *, entry_count=2, desired_sum=2020):
     return prod(*pairing)
 
 
-def part1(expenses):
+def part1(expenses: List[int]) -> int:
     return calculate_report(expenses)
 
 
-def part2(expenses):
+def part2(expenses: List[int]) -> int:
     return calculate_report(expenses, entry_count=3)
 
 
-def main():
+def main() -> None:
     with fileinput.input() as input:
         lines = [int(line) for line in input]
 
