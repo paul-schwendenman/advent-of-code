71dfa83e39aa0b00e2a68d5e69881ed5bfa07789
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-05 01:08:16 -0500

    Add tests for find_seat_id

--- 2020/day05/test_day05.py
+++ 2020/day05/test_day05.py
@@ -1,11 +1,27 @@
 import pytest
-from day05 import part1, part2, readfile
+from day05 import find_seat_id, part1, part2, readfile
 
 
 @pytest.fixture
 def input_data():
     with readfile('input') as data:
         yield data
+
+
+def test_find_seat_id_357():
+    assert find_seat_id('FBFBBFFRLR') == 357
+
+
+def test_find_seat_id_567():
+    assert find_seat_id('BFFFBBFRRR') == 567
+
+
+def test_find_seat_id_119():
+    assert find_seat_id('FFFBBBFRRR') == 119
+
+
+def test_find_seat_id_820():
+    assert find_seat_id('BBFFBBFRLL') == 820
 
 
 def test_part1_sample(input_data):
