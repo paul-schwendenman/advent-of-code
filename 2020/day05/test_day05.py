f771c78aa0e75c89fba7253579066e8c799d54bd
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-05 01:02:11 -0500

    Add tests for input file

--- 2020/day05/test_day05.py
+++ 2020/day05/test_day05.py
@@ -3,14 +3,14 @@ from day05 import part1, part2, readfile
 
 
 @pytest.fixture
-def sample_data():
-    with readfile('sample') as data:
+def input_data():
+    with readfile('input') as data:
         yield data
 
 
-def test_part1_sample(sample_data):
-    assert part1(sample_data) == 2
+def test_part1_sample(input_data):
+    assert part1(input_data) == 861
 
 
-def test_part2_sample(sample_data):
-    assert part2(sample_data) == 2
+def test_part2_sample(input_data):
+    assert part2(input_data) == 633
