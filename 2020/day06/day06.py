a9921812c428fc3bcf3760d24b229ede3e00e702
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-06 01:41:14 -0500

    Refactor to extract common core of part 1 and 2

--- 2020/day06/day06.py
+++ 2020/day06/day06.py
@@ -1,6 +1,6 @@
 from contextlib import contextmanager
 from functools import reduce
-from typing import Iterable, List
+from typing import Callable, Iterable, List
 import sys
 
 
@@ -20,12 +20,16 @@ def parse_answers(group: str) -> Iterable[set]:
     return (set(person) for person in group.split('\n') if person)
 
 
+def solve(groups: List[str], func: Callable[[set, set], set]) -> int:
+    return sum(len(reduce(func, parse_answers(group))) for group in groups)
+
+
 def part1(groups: List[str]) -> int:
-    return sum(len(reduce(set.union, parse_answers(group))) for group in groups)
+    return solve(groups, set.union)
 
 
 def part2(groups: List[str]) -> int:
-    return sum(len(reduce(set.intersection, parse_answers(group))) for group in groups)
+    return solve(groups, set.intersection)
 
 
 def main() -> None:
