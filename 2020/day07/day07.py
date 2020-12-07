4dfb2a3359bfcb7eff2de79266a651a06a929b10
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-07 00:35:04 -0500

    Add part 1 solution

--- 2020/day07/day07.py
+++ 2020/day07/day07.py
@@ -1,4 +1,6 @@
 from contextlib import contextmanager
+from collections import defaultdict
+from functools import reduce
 from typing import List
 import fileinput
 
@@ -9,8 +11,50 @@ def readfile(filename=None):
         yield [line.rstrip() for line in data]
 
 
+def strip_quanity(raw_bag):
+    return raw_bag.split(' ', 1)[1]
+
+
+def strip_bags(raw_bag):
+    return raw_bag.strip().replace('bags', 'bag')
+
+
+def parse_rule(raw_rule):
+    parent, raw_children = raw_rule[:-1].split('contain')
+
+    children = [strip_bags(child) for child in raw_children.split(',')]
+
+    if children == ["no other bag"]:
+        return None
+
+    return strip_bags(parent), children
+
+def search(item, tree):
+    if item not in tree:
+        return set()
+
+    return reduce(set.union, (search(parent, tree) for parent in tree[item]), set(tree[item]))
+
+
 def part1(data: List[str]) -> int:
-    return 1
+    rules = defaultdict(list)
+    for raw_rule in data:
+        rule = parse_rule(raw_rule)
+
+        if rule:
+            # print(rule)
+            parent, children = rule
+
+            for child in children:
+                # print(strip_quanity(child))
+                rules[strip_quanity(child)].append(parent)
+
+    # print(rules.keys())
+    # print(len(rules['shiny gold bag']))
+
+    count = 0
+
+    return len(search('shiny gold bag', rules))
 
 
 def part2(data: List[str]) -> int:
@@ -20,7 +64,7 @@ def part2(data: List[str]) -> int:
 def main() -> None:
     with readfile() as data:
         print(part1(data))
-        print(part2(data))
+        # print(part2(data))
 
 
 if __name__ == '__main__':
