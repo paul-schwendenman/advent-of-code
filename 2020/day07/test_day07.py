e002e5bc540dc1e37cb5d62aedabcea571968a93
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-07 00:46:19 -0500

    Add tests for part1 and part2

--- 2020/day07/test_day07.py
+++ 2020/day07/test_day07.py
@@ -8,9 +8,19 @@ def sample_data():
         yield data
 
 
+@pytest.fixture
+def sample2_data():
+    with readfile('sample2') as data:
+        yield data
+
+
 def test_part1_sample(sample_data):
-    assert part1(sample_data) == 1
+    assert part1(sample_data) == 4
 
 
 def test_part2_sample(sample_data):
-    assert part2(sample_data) == 1
+    assert part2(sample_data) == 32
+
+
+def test_part2_sample2(sample2_data):
+    assert part2(sample2_data) == 126
