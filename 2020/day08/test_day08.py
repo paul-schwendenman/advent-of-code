c79b8201fd2ea23e0625f892855cf3c62088a033
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-09 01:10:15 -0500

    Add tests to day 8 based on input file

--- 2020/day08/test_day08.py
+++ 2020/day08/test_day08.py
@@ -8,9 +8,23 @@ def sample_data():
         yield data
 
 
+@pytest.fixture
+def input_data():
+    with readfile('input') as data:
+        yield data
+
+
 def test_part1_sample(sample_data):
     assert part1(sample_data) == 5
 
 
 def test_part2_sample(sample_data):
     assert part2(sample_data) == 8
+
+
+def test_part1_input(input_data):
+    assert part1(input_data) == 1818
+
+
+def test_part2_input(input_data):
+    assert part2(input_data) == 631
