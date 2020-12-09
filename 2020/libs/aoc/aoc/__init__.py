c2793ef4b9143f0b99899542d509859d45af74eb
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-08 23:43:17 -0500

    Add skeleton for lib/

---
+++ 2020/libs/aoc/aoc/__init__.py
@@ -0,0 +1,12 @@
+'''Advent Of Code Helpers
+
+An implementation advent of code helper methods
+'''
+
+__author__ = 'Paul Schwendenman'
+__email__ = 'schwendenman.paul+aoc@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+
+
+__all__ = []
---
+++ 2020/libs/aoc/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright 2019 Paul Schwendenman. All Rights Reserved.
+
+from setuptools import setup
+import aoc
+
+
+setup(name='aoc',
+      version=aoc.__version__,
+      description=aoc.__doc__.strip(),
+      long_description='',
+      author=aoc.__author__,
+      author_email=aoc.__email__,
+      license=aoc.__license__,
+      url='https://github.com/paul-schwendenman/advent-of-code/libs/aoc/',
+      packages=['aoc'],
+      package_data={"aoc": ["py.typed"]},
+      classifiers=[],
+      )
