0a27ae61357b911e6007d4dc8583012b4df66a08
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-08 23:50:05 -0500

    Add readfile to lib

--- 2020/libs/aoc/aoc/__init__.py
+++ 2020/libs/aoc/aoc/__init__.py
@@ -2,6 +2,9 @@
 
 An implementation advent of code helper methods
 '''
+from contextlib import contextmanager
+from typing import Any, List
+import fileinput
 
 __author__ = 'Paul Schwendenman'
 __email__ = 'schwendenman.paul+aoc@gmail.com'
@@ -9,4 +12,10 @@ __license__ = 'MIT'
 __version__ = '0.1.0'
 
 
-__all__ = []
+@contextmanager
+def readfile(filename=None):
+    with fileinput.input(filename) as data:
+        yield [line.rstrip() for line in data]
+
+
+__all__: List[Any] = [readfile]
