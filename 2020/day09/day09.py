b297c84d9a067865b9d1a0f4b48c5e7ed8748f38
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-09 00:33:25 -0500

    Add day9 part2

--- 2020/day09/day09.py
+++ 2020/day09/day09.py
@@ -18,13 +18,32 @@ def part1(data: List[str], preamble: int = 25) -> int:
     raise ValueError()
 
 
-def part2(data: List[str]) -> int:
-    pass
+def part2(data: List[str], goal=556543474) -> int:
+    numbers = [int(num) for num in data]
+    bounds = None
+
+    for index, number in enumerate(numbers):
+        pos = index
+        sum = 0
+        while sum < goal and pos < len(numbers):
+            sum += numbers[pos]
+            pos += 1
+
+        if sum == goal:
+            bounds = (index, pos)
+            break
+    else:
+        raise ValueError()
+
+    weak_range = numbers[bounds[0]:bounds[1]]
+
+    return min(weak_range) + max(weak_range)
 
 
 def main() -> None:
     with readfile() as data:
-        print(part1(data, 25))
+        # print(part1(data, 25))
+        # print(part2(data, 127))
         print(part2(data))
 
 
