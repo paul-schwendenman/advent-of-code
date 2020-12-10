7ee1e0ce3d0bc7c2df4d675b356ff32912df3972
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-10 01:05:34 -0500

    Add day 10 solution

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -1,18 +1,72 @@
 from typing import List
+from collections import defaultdict
 from aoc import readfile
 
 
+# def next_adapter():
+
+
 def part1(data: List[str]) -> int:
-    pass
+    adapters = [0] + sorted([int(item) for item in data])
+
+    counts = defaultdict(int)
+
+    print(adapters)
+
+    for index, adapter in enumerate(adapters[:-1]):
+        counts[adapters[index+1] - adapter] += 1
+
+    print(counts)
+
+    return counts[1] * (counts[3] + 1)
 
 
-def part2(data: List[str]) -> int:
-    pass
+def arrangements(adapters, base, goal):
+    if base == goal:
+        return 1
+    if base not in adapters:
+        return 0
+
+    return arrangements(adapters, base+1, goal) + arrangements(adapters, base+2, goal) + arrangements(adapters, base+3, goal)
+
+
+def part2_slow(data: List[str]) -> int:
+    adapters = [0] + sorted([int(item) for item in data])
+
+    goal = max(adapters) + 3
+
+    return arrangements(adapters, 0, goal)
+
+
+def part2(data):
+    adapters = [0] + sorted([int(item) for item in data])
+
+    adapters.append(adapters[-1] + 3)
+
+    small = []
+    ways = 1
+
+    for index, adapter in enumerate(adapters[:-1]):
+        if adapters[index+1] - adapter == 3:
+            small.append(adapter)
+            sub_ways = arrangements(small, small[0], small[-1])
+            print(small, sub_ways)
+            if sub_ways:
+                ways *= sub_ways
+            small = [adapter]
+        else:
+            small.append(adapter)
+
+    print(ways)
+
+    # print(diffs)
+
+    return None
 
 
 def main() -> None:
     with readfile() as data:
-        print(part1(data))
+        # print(part1(data))
         print(part2(data))
 
 
