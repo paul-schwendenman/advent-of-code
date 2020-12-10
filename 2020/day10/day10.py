f5ca8ef9f242ca8d14e45f4863d24db333c3172e
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-09 22:41:46 -0500

    Add skeleton for day10

---
+++ 2020/day10/day10.py
@@ -0,0 +1,20 @@
+from typing import List
+from aoc import readfile
+
+
+def part1(data: List[str]) -> int:
+    pass
+
+
+def part2(data: List[str]) -> int:
+    pass
+
+
+def main() -> None:
+    with readfile() as data:
+        print(part1(data))
+        print(part2(data))
+
+
+if __name__ == '__main__':
+    main()
