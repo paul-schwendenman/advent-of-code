5b6f52def4db2c16f1a0e690b169aa5eb1f042d6
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-11 00:49:55 -0500

    Add tests for parts 1 and 2

--- 2020/day11/test_day11.py
+++ 2020/day11/test_day11.py
@@ -2,16 +2,16 @@ from day11 import part1, part2
 
 
 def test_part1_sample(sample_data):
-    assert part1(sample_data) == -1
+    assert part1(sample_data) == 37
 
 
 def test_part2_sample(sample_data):
-    assert part2(sample_data) == -1
+    assert part2(sample_data) == 26
 
 
 def test_part1_input(input_data):
-    assert part1(input_data) == -1
+    assert part1(input_data) == 2261
 
 
 def test_part2_input(input_data):
-    assert part2(input_data) == -1
+    assert part2(input_data) == 2039
