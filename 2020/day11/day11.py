a51115d5f45e9ec74e41f2f56414a3b6d159f6fc
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-11 00:32:50 -0500

    Add part2 solution

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -71,6 +71,58 @@ def part1(data: List[str]) -> int:
 
 
 def part2(data: List[str]) -> int:
+    x_max = len(data)
+    y_max = len(data[0])
+
+    grid = parse_grid(data)
+
+    last_seat_count = 0
+    seat_count = 0
+
+    while True:
+        new_grid = defaultdict(str)
+
+        for x_index in range(x_max):
+            for y_index in range(y_max):
+                here = Point(x_index, y_index)
+                current = grid[here]
+
+                if current == '.':
+                    new_grid[here] = '.'
+                    continue
+
+                count = 0
+
+                for direction in Point(0, 0).get_neighboors():
+                    next_seat = here + direction
+                    while grid[next_seat] != '':
+                        if grid[next_seat] == '.':
+                            next_seat += direction
+                        elif grid[next_seat] == '#':
+                            count += 1
+                            break
+                        elif grid[next_seat] == 'L':
+                            break
+
+                # count = sum(grid[neighboor] == '#' for neighboor in here.get_neighboors())
+
+                if count >= 5:
+                    new_grid[here] = 'L'
+                elif count == 0:
+                    new_grid[here] = '#'
+                else:
+                    new_grid[here] = current
+
+        seat_count = sum(seat == '#' for _, seat in new_grid.items())
+
+        if last_seat_count == seat_count:
+            break
+
+        last_seat_count = seat_count
+        grid = new_grid
+
+    return seat_count
+
     pass
 
 
