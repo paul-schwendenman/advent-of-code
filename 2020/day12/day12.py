e6d2cdb05c69e9e281067710682a05e6d6b6547d
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-12 00:45:17 -0500

    Refactor to use Enum

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -1,7 +1,26 @@
 from __future__ import annotations
 from typing import List
 from aoc import readfile
-# from math import
+from enum import Enum
+
+
+class Action(Enum):
+    FORWARD = 'F'
+    NORTH = 'N'
+    EAST = 'E'
+    SOUTH = 'S'
+    WEST = 'W'
+    RIGHT = 'R'
+    LEFT = 'L'
+
+
+def map_angle_to_action(angle: int) -> Action:
+    return {
+        0: Action.EAST,
+        90: Action.NORTH,
+        180: Action.WEST,
+        270: Action.SOUTH,
+    }[angle % 360]
 
 
 def part1(data: List[str]) -> int:
@@ -10,27 +29,22 @@ def part1(data: List[str]) -> int:
     theta = 0
 
     for command in data:
-        action, num = command[0], int(command[1:])
+        action, num = Action(command[0]), int(command[1:])
 
-        if action == 'F':
-            action = {
-                0: 'E',
-                90: 'N',
-                180: 'W',
-                270: 'S'
-            }[theta % 360]
+        if action == Action.FORWARD:
+            action = map_angle_to_action(theta)
 
-        if action == 'N':
+        if action == Action.NORTH:
             y -= num
-        elif action == 'S':
+        elif action == Action.SOUTH:
             y += num
-        elif action == 'W':
+        elif action == Action.WEST:
             x -= num
-        elif action == 'E':
+        elif action == Action.EAST:
             x += num
-        elif action == 'L':
+        elif action == Action.LEFT:
             theta += num
-        elif action == 'R':
+        elif action == Action.RIGHT:
             theta -= num
 
     return abs(x) + abs(y)
@@ -43,21 +57,21 @@ def part2(data: List[str]) -> int:
     w_y = 1
 
     for command in data:
-        action, num = command[0], int(command[1:])
+        action, num = Action(command[0]), int(command[1:])
 
-        if action == 'F':
+        if action == Action.FORWARD:
             x += w_x * num
             y += w_y * num
 
-        if action == 'N':
+        if action == Action.NORTH:
             w_y += num
-        elif action == 'S':
+        elif action == Action.SOUTH:
             w_y -= num
-        elif action == 'W':
+        elif action == Action.WEST:
             w_x -= num
-        elif action == 'E':
+        elif action == Action.EAST:
             w_x += num
-        elif action == 'L':
+        elif action == Action.LEFT:
             if num == 90:
                 w_y, w_x = w_x, -w_y
             elif num == 180:
@@ -65,7 +79,7 @@ def part2(data: List[str]) -> int:
             elif num == 270:
                 w_y, w_x = -w_x, w_y
 
-        elif action == 'R':
+        elif action == Action.RIGHT:
             if num == 90:
                 w_y, w_x = -w_x, w_y
             elif num == 180:
