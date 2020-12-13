f07e03163fd3a52b022a1c4c2a40643eb2e11335
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-13 01:20:32 -0500

    Add sample and input files

--- 2020/day13/test_day13.py
+++ 2020/day13/test_day13.py
@@ -9,9 +9,9 @@ def test_part2_sample(sample_data):
     assert part2(sample_data) == -1
 
 
-def test_part1_input(input_data):
-    assert part1(input_data) == -1
+# def test_part1_input(input_data):
+#     assert part1(input_data) == -1
 
 
-def test_part2_input(input_data):
-    assert part2(input_data) == -1
+# def test_part2_input(input_data):
+#     assert part2(input_data) == -1
