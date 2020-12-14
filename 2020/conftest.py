81afe262685c474b812e29865a83961612b3cbd8
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-14 00:58:29 -0500

    Add sample2 fixture

--- 2020/conftest.py
+++ 2020/conftest.py
@@ -9,6 +9,12 @@ def sample_data():
 
 
 @pytest.fixture
+def sample2_data():
+    with readfile('sample2') as data:
+        yield data
+
+
+@pytest.fixture
 def input_data():
     with readfile('input') as data:
         yield data
