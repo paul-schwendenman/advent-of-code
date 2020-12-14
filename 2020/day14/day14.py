8336e6e9aea5a1b9bf0ad945b38b66398c68679a
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-13 23:14:42 -0500

    Add skeleton for day14

---
+++ 2020/day14/day14.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import List
+from aoc import readfile
+
+
+def part1(data: List[str]) -> int:
+    pass
+
+
+def part2(data: List[str]) -> int:
+    pass
+
+
+def main() -> None:
+    with readfile() as data:
+        print(part1(data))
+        print(part2(data))
+
+
+if __name__ == '__main__':
+    main()
