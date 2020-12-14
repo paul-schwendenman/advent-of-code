6a918624e24a506268160d802e610629693ac203
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-14 00:55:55 -0500

    Cleanup solutions

--- 2020/day14/day14.py
+++ 2020/day14/day14.py
@@ -2,57 +2,51 @@ from __future__ import annotations
 from typing import List
 from aoc import readfile
 from collections import defaultdict
-import re
 
 
-def convert_memory(value):
+def convert_memory(value: int) -> str:
     return "{0:036b}".format(value)
 
 
-def apply_mask_to_value(value, mask):
-    value = convert_memory(value)
-    return ''.join(a if b == 'X' else b for a, b in zip(value, mask))
+def apply_mask_to_value(value: int, mask: str) -> str:
+    new_value = convert_memory(value)
+    return ''.join(a if b == 'X' else b for a, b in zip(new_value, mask))
+
 
 def part1(data: List[str]) -> int:
     # memory = [0] * 37
     memory = defaultdict(int)
     mask = None
     for line in data:
-        print(line)
         if line[:3] == 'mem':
             s = line.split('] = ')
             address = int(s[0].split('[')[1])
             value = int(s[1])
-            print(address, value)
-            print(convert_memory(value))
-            print(apply_mask_to_value(value, mask))
             memory[address] = int(apply_mask_to_value(value, mask), base=2)
         else:
             mask = line[7:]
-            print(mask)
-
 
     return sum(i[1] for i in memory.items())
 
 
-def map_bit(bit, mask):
+def map_bit(bit: str, mask: str) -> str:
     if mask == '0':
         return bit
     elif mask == '1':
         return '1'
     elif mask == 'X':
         return 'X'
+    raise ValueError('Expected 1, 0, or X')
 
 
-def apply_mask_to_address(address, mask):
+def apply_mask_to_address(address: int, mask: str) -> str:
     value = convert_memory(address)
     return ''.join(map_bit(a, b) for a, b in zip(value, mask))
 
 
-def floating_addresses(address):
+def floating_addresses(address: str) -> List[str]:
     if not address:
         return ['']
-    # print(address[0], address[1:])
     if address[0] in '01':
         return [address[0] + part for part in floating_addresses(address[1:])]
     else:
@@ -62,32 +56,23 @@ def floating_addresses(address):
 def part2(data: List[str]) -> int:
     # memory = [0] * 37
     memory = defaultdict(int)
-    mask = None
+    mask = ''
     for line in data:
-        print(line)
         if line[:3] == 'mem':
             s = line.split('] = ')
             address = int(s[0].split('[')[1])
             value = int(s[1])
-            print(address, value)
-            print(convert_memory(address))
-            print(apply_mask_to_address(address, mask))
             for new_address in floating_addresses(apply_mask_to_address(address, mask)):
                 memory[new_address] = value
-
-            # memory[address] = int(apply_mask_to_value(value, mask), base=2)
         else:
             mask = line[7:]
-            print(mask)
-
 
     return sum(i[1] for i in memory.items())
-    pass
 
 
 def main() -> None:
     with readfile() as data:
-        # print(part1(data))
+        print(part1(data))
         print(part2(data))
 
 
