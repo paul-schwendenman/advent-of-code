499fa5281ada7704a3067fa774e189dd396ef31a
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-14 00:58:43 -0500

    Add day 14 tests

--- 2020/day14/test_day14.py
+++ 2020/day14/test_day14.py
@@ -2,16 +2,16 @@ from day14 import part1, part2
 
 
 def test_part1_sample(sample_data):
-    assert part1(sample_data) == 0
+    assert part1(sample_data) == 165
 
 
-def test_part2_sample(sample_data):
-    assert part2(sample_data) == 0
+def test_part2_sample(sample2_data):
+    assert part2(sample2_data) == 208
 
 
 def test_part1_input(input_data):
-    assert part1(input_data) == 0
+    assert part1(input_data) == 14553106347726
 
 
 def test_part2_input(input_data):
-    assert part2(input_data) == 0
+    assert part2(input_data) == 2737766154126
