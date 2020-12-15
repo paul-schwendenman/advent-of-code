f88f547cf61a046fe6247100f1de3f46fcdf91cd
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-15 01:12:26 -0500

    Add part 2 solution

--- 2020/day15/day15.py
+++ 2020/day15/day15.py
@@ -8,36 +8,44 @@ def part1(data: str, turns: int = 2020) -> int:
     start = [int(num) for num in data.split(',')]
     counter = defaultdict(list)
 
+    # sequence = []
+
     turn = 0
 
     for item in start:
         turn += 1
         counter[item].append(turn)
+        # sequence.append(item)
 
     last_num = item
 
     while turn < turns:
+        if turn % 3000000 == 0:
+            print(turn)
         if len(counter[last_num]) == 1:
             last_num = 0
         else:
             second_last, last = counter[last_num][-2:]
-            print(f'{last} - {second_last} = {last - second_last}')
+            # print(f'{last} - {second_last} = {last - second_last}')
             last_num = last - second_last
 
         turn += 1
         counter[last_num].append(turn)
-        print(turn, last_num)
+        # sequence.append(last_num)
+        # print(turn, last_num)
+
+    # print(sequence[:30])
 
     return last_num
 
 
-def part2(data: List[str]) -> int:
-    pass
+def part2(data: str) -> int:
+    return part1(data, 30000000)
 
 
 def main() -> None:
     print(part1("6,3,15,13,1,0"))
-    # print(part1(data))
+    print(part2("6,3,15,13,1,0"))
 
 
 if __name__ == '__main__':
