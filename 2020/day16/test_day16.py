b1cf8e1eba401b6c1760ac404233eae3512b218e
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-16 01:23:33 -0500

    Add tests for refactoring later

---
+++ 2020/day16/test_day16.py
@@ -0,0 +1,9 @@
+from day16 import part1, part2
+
+
+def test_part1_input(input_data):
+    assert part1(input_data) == 23036
+
+
+def test_part2_input(input_data):
+    assert part2(input_data) == 1909224687553
