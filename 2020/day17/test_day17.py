4e13c27f35324104229a1e5db7ac9cbca58b3d78
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-16 23:51:29 -0500

    Add skeleton for day 17

---
+++ 2020/day17/test_day17.py
@@ -0,0 +1,17 @@
+from day17 import part1, part2
+
+
+def test_part1_sample(sample_data):
+    assert part1(sample_data) == 0
+
+
+def test_part2_sample(sample_data):
+    assert part2(sample_data) == 0
+
+
+def test_part1_input(input_data):
+    assert part1(input_data) == 0
+
+
+def test_part2_input(input_data):
+    assert part2(input_data) == 0
