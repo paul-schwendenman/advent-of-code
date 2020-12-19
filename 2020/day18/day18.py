73ceb86ca77e3e34c79c0a915a6398494956fec3
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-18 23:41:01 -0500

    Cleanup code for day 18

--- 2020/day18/day18.py
+++ 2020/day18/day18.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
-from typing import DefaultDict, Iterator, Sequence
+from typing import Sequence
 from aoc import readfile
-from collections import defaultdict, namedtuple
 
 
-def parse_math(line: str):
-    print(f'\nline: {line}')
+def parse_math(line: str) -> str:
     if line[0] == '(':
         parens = 0
         for index, char in enumerate(line):
@@ -15,14 +13,10 @@ def parse_math(line: str):
                 parens -= 1
             if parens == 0:
                 break
-        # print(f'front: "{line[1:index]}"')
-        print(f'back: "{line[index+1:]}"')
-        print(f'parse_math(parse_math({line[1:index]}){line[index+1:]})')
         return parse_math(f'{parse_math(line[1:index])}{line[index+1:]}')
     parts = line.split(' ', 2)
 
     if len(parts) == 1:
-        print(f'"{parts[0]}"')
         return parts[0]
     left, opp, rest = parts
 
@@ -35,45 +29,27 @@ def parse_math(line: str):
                 parens -= 1
             if parens == 0:
                 break
-        # print(f'front: "{line[1:index]}"')
-        print(f'''parse_math(parse_math({left} {opp} {f'parse_math({rest[1:index]})'}){rest[index+1:]})''')
         return parse_math(f"{parse_math(f'{left} {opp} {parse_math(rest[1:index])}')}{rest[index+1:]}")
 
     rest_parts = rest.split(' ', 1)
     if len(rest_parts) == 1:
         if opp == '*':
-            print(f'{int(left) * int(rest_parts[0])}')
             return f'{int(left) * int(rest_parts[0])}'
         elif opp == '+':
-            print(f'{int(left) + int(rest_parts[0])}')
             return f'{int(left) + int(rest_parts[0])}'
     else:
         if opp == '*':
-            print(f"parse_math({int(left) * int(rest_parts[0])} {rest_parts[1]})")
             return parse_math(f'{int(left) * int(rest_parts[0])} {rest_parts[1]}')
         elif opp == '+':
-            print(f"parse_math({int(left) + int(rest_parts[0])} {rest_parts[1]})")
             return parse_math(f'{int(left) + int(rest_parts[0])} {rest_parts[1]}')
+        else:
+            raise ValueError(f'Invalid operator {opp}')
 
-    pass
-
-
-    # print(line)
-    # if line[0] == '(':
-    #     parens = 1
-    #     for index, char in enumerate(line[1:]):
-    #         if char == '(':
-    #             parens += 1
-    #         else char == ')':
-    #             parens -= 1
-    #         if parens == 0:
-    #             parse_math(1:index)
 
 def part1(data: Sequence[str]) -> int:
     answers = []
     for line in data:
         answer = parse_math(line)
-        print(f'{line} = {answer}')
         answers.append(int(answer))
 
     return sum(answers)
