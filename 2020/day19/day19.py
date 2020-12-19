06d4dc438abd33705a24ef400d04f4000ec6eb8e
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-19 18:38:43 -0500

    WIP Add part2 regex solution

--- 2020/day19/day19.py
+++ 2020/day19/day19.py
@@ -3,6 +3,7 @@ from typing import Sequence, Mapping
 from itertools import product
 from functools import lru_cache
 from aoc import readfile, tracer, profiler
+import re
 
 
 def parse_rules(rules: Sequence[str]) -> Mapping[str, str]:
@@ -30,9 +31,35 @@ def eval_rule(rules, rule_id):
             return list(''.join(item) for item in product(*[inner(rules[item]) for item in rule.split(' ')]))
 
     print(inner.cache_info())
-    value =  inner(rules[rule_id])
+    value = inner(rules[rule_id])
     print(inner.cache_info())
     return value
+
+
+def convert_rule(rules, rule_id):
+    @lru_cache(maxsize=1000)
+    # @tracer
+    def inner(rule_id=None, rule=None):
+        if rule is None:
+            rule = rules[rule_id]
+        if rule == '"a"':
+            return 'a'
+        elif rule == '"b"':
+            return 'b'
+        elif rule_id == '8':
+            return f'({inner("42")})+'
+        elif rule_id == '11':
+            return f'({inner("42")})+({inner("31")})+'
+        elif '|' in rule:
+            first, second = rule.split(' | ')
+            return f'(({inner(rule_id, rule=first)})|({inner(rule_id, rule=second)}))'
+        else:
+            return ''.join(inner(rule_id=item) for item in rule.split(' '))
+
+    print(inner.cache_info())
+    value = inner(rule_id)
+    print(inner.cache_info())
+    return f'^{value}$'
 
 
 @profiler
@@ -46,11 +73,22 @@ def part1(data: Sequence[str]) -> int:
 
     return sum(message in valid_answers for message in messages)
 
-    # print(list(valid_answers))
 
-
+@profiler
 def part2(data: Sequence[str]) -> int:
-    pass
+    section_break = data.index('')
+    raw_rules, messages = data[:section_break], data[section_break+1:]
+
+    rules = parse_rules(raw_rules)
+    rules['8'] = '42 | 42 8'
+    rules['11'] = '42 31 | 42 11 31'
+
+    # valid_answers = set(eval_rule(rules, '0'))
+    print(convert_rule(rules, '0'))
+    pattern = re.compile(convert_rule(rules, '0'))
+    print(pattern)
+
+    return sum(1 for message in messages if re.match(pattern, message))
 
 
 def main() -> None:
