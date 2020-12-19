ac51e0c02a20958b0d76f8058a4d3af788875062
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-19 01:51:41 -0500

    Add part 1 solution

---
+++ 2020/day19/day19.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+from typing import Sequence, Mapping
+from itertools import product
+from functools import lru_cache
+from aoc import readfile, tracer
+
+
+def parse_rules(rules: Sequence[str]) -> Mapping[str, str]:
+    mapping = {}
+
+    for rule in rules:
+        key, value = rule.split(': ')
+        mapping[key] = value
+    return mapping
+
+
+# @tracer
+def eval_rule(rules, rule_id):
+    @lru_cache(maxsize=10)
+    # @tracer
+    def inner(rule):
+        if rule == '"a"':
+            return ['a']
+        elif rule == '"b"':
+            return ['b']
+        elif '|' in rule:
+            first, second = rule.split(' | ')
+            return [item for group in (inner(first), inner(second)) for item in group]
+        else:
+            return list(''.join(item) for item in product(*[inner(rules[item]) for item in rule.split(' ')]))
+    return inner(rules[rule_id])
+
+
+def part1(data: Sequence[str]) -> int:
+    section_break = data.index('')
+    raw_rules, messages = data[:section_break], data[section_break+1:]
+
+    rules = parse_rules(raw_rules)
+
+    print(rules)
+
+    valid_answers = eval_rule(rules, '0')
+
+    return sum(message in valid_answers for message in messages)
+
+    # print(list(valid_answers))
+
+
+def part2(data: Sequence[str]) -> int:
+    pass
+
+
+def main() -> None:
+    with readfile() as data:
+        print(part1(data))
+        print(part2(data))
+
+
+if __name__ == '__main__':
+    main()
