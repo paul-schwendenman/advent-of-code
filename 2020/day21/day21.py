bb1f206d74a8546ca94d7e6bafaca37c3626ac2e
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-21 01:43:59 -0500

    Refactor day21

--- 2020/day21/day21.py
+++ 2020/day21/day21.py
@@ -1,85 +1,69 @@
 from __future__ import annotations
-from typing import Sequence, Mapping, MutableMapping
-from collections import defaultdict
-from itertools import product
-from functools import lru_cache, reduce
-from aoc import readfile, tracer, profiler
+from typing import List, Sequence, Mapping, MutableMapping, Tuple
+from functools import reduce
+from aoc import readfile, profiler
 import re
 
 
-def parse_ingredients(line: str):
-    ingredients, allergens = re.match(r'(.*) \(contains (.*)\)', line).groups()
-    ingredients = set(ingredients.split(' '))
-    allergens = set(allergens.split(', '))
+def parse_ingredients(line: str) -> Tuple[set, set]:
+    match = re.match(r'(.*) \(contains (.*)\)', line)
+
+    if match:
+        raw_ingredients, raw_allergens = match.groups()
+    else:
+        raise ValueError("Invalid line:", line)
+
+    ingredients = set(raw_ingredients.split(' '))
+    allergens = set(raw_allergens.split(', '))
     return ingredients, allergens
+
+
+def generate_allergen_map(labels: List[Tuple[set, set]]) -> Mapping[str, set]:
+    allergen_map: MutableMapping[str, set] = {}
+    for ingredients, allergens in labels:
+        for allergen in allergens:
+            if allergen not in allergen_map:
+                allergen_map[allergen] = ingredients
+            else:
+                allergen_map[allergen] = allergen_map[allergen].intersection(ingredients)
+    return allergen_map
 
 
 @profiler
 def part1(data: Sequence[str]) -> int:
-    labels = []
-    ingredient_map = {}
-    allergen_map: MutableMapping[str, set] = {}
-
-    for line in data:
-        labels.append(parse_ingredients(line))
+    labels = [parse_ingredients(line) for line in data]
+    allergen_map = generate_allergen_map(labels)
 
     all_ingredients = reduce(set.union, [ingredients for ingredients, _ in labels])
 
-    for ingredients, allergens in labels:
-        # for ingredient in ingredients:
-        #     pass
-        for allergen in allergens:
-            if allergen not in allergen_map:
-                allergen_map[allergen] = ingredients
-            else:
-                allergen_map[allergen] = allergen_map[allergen].intersection(ingredients)
-
-    print(allergen_map)
     all_possible_allergens = reduce(set.union, [ingredients for ingredients in allergen_map.values()])
 
     clean_ingredients = (all_ingredients - all_possible_allergens)
-    print(clean_ingredients)
 
     return sum([len(ingredients.intersection(clean_ingredients)) for ingredients, _ in labels])
 
 
-
 @profiler
 def part2(data: Sequence[str]) -> str:
-    labels = []
-    allergen_map: MutableMapping[str, set] = {}
-
-    for line in data:
-        labels.append(parse_ingredients(line))
+    labels = [parse_ingredients(line) for line in data]
+    allergen_map = generate_allergen_map(labels)
 
     all_ingredients = reduce(set.union, [ingredients for ingredients, _ in labels])
 
-    for ingredients, allergens in labels:
-        for allergen in allergens:
-            if allergen not in allergen_map:
-                allergen_map[allergen] = ingredients
-            else:
-                allergen_map[allergen] = allergen_map[allergen].intersection(ingredients)
-
     all_possible_allergens = reduce(set.union, [ingredients for ingredients in allergen_map.values()])
 
-    clean_ingredients = (all_ingredients - all_possible_allergens)
+    used_ingredients = (all_ingredients - all_possible_allergens)
 
-    canonical = {}
+    final = {}
 
-    for allergen in allergen_map.keys():
-        canonical[allergen] = (allergen_map[allergen] - clean_ingredients)
-
-    used_ingredients = clean_ingredients.copy()
-
-    while len(used_ingredients) < len(all_ingredients):
-        for allergen, ingredients in canonical.items():
+    while all_ingredients - used_ingredients:
+        for allergen, ingredients in allergen_map.items():
             remaining = ingredients - used_ingredients
             if len(remaining) == 1:
-                canonical[allergen] = remaining
                 used_ingredients = used_ingredients.union(remaining)
+                final[allergen] = remaining.pop()
 
-    return ','.join([canonical[allergen].pop() for allergen in sorted(allergen_map.keys())])
+    return ','.join([final[allergen] for allergen in sorted(allergen_map.keys())])
 
 
 def main() -> None:
