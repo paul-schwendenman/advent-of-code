f88de98e368a7c74055a0847465074e99718c008
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-22 00:14:27 -0500

    Add part 1

--- 2020/day22/day22.py
+++ 2020/day22/day22.py
@@ -1,10 +1,32 @@
 from __future__ import annotations
 from typing import Sequence
 from aoc import readfile
+from collections import deque
 
 
 def part1(data: Sequence[str]) -> int:
-    pass
+    player_1, player_2 = [deque(int(card) for card in player.split()) for player in '\n'.join(data[1:]).split('\n\nPlayer 2:\n')]
+
+    while len(player_1) > 0 and len(player_2) > 0:
+        card_1 = player_1.popleft()
+        card_2 = player_2.popleft()
+
+        if card_1 > card_2:
+            player_1.append(card_1)
+            player_1.append(card_2)
+        elif card_1 < card_2:
+            player_2.append(card_2)
+            player_2.append(card_1)
+
+    print(player_1)
+    print(player_2)
+    if len(player_2) == 0:
+        winner = player_1
+    else:
+        winner = player_2
+
+    return sum((a + 1) * b for a, b in enumerate(reversed(winner)))
+
 
 
 def part2(data: Sequence[str]) -> int:
--- 2020/day22/test_day22.py
+++ 2020/day22/test_day22.py
@@ -2,7 +2,7 @@ from day22 import part1, part2
 
 
 def test_part1_sample(sample_data):
-    assert part1(sample_data) == 0
+    assert part1(sample_data) == 306
 
 
 def test_part2_sample(sample_data):
@@ -10,7 +10,7 @@ def test_part2_sample(sample_data):
 
 
 def test_part1_input(input_data):
-    assert part1(input_data) == 0
+    assert part1(input_data) == 33694
 
 
 def test_part2_input(input_data):
