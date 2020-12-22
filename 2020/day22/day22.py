7e66ab73dd8f2a25319c9d7d5cf9f28004d0b8d7
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-22 00:47:10 -0500

    Add part2 for sample

--- 2020/day22/day22.py
+++ 2020/day22/day22.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 from typing import Sequence
-from aoc import readfile
+from aoc import readfile, tracer
 from collections import deque
 
 
-def part1(data: Sequence[str]) -> int:
-    player_1, player_2 = [deque(int(card) for card in player.split()) for player in '\n'.join(data[1:]).split('\n\nPlayer 2:\n')]
+def play_combat(player_1, player_2):
 
     while len(player_1) > 0 and len(player_2) > 0:
         card_1 = player_1.popleft()
@@ -22,15 +21,69 @@ def part1(data: Sequence[str]) -> int:
     print(player_2)
     if len(player_2) == 0:
         winner = player_1
+        winner_name = '1'
     else:
+        winner_name = '2'
         winner = player_2
+
+    return winner_name, winner
+
+
+@tracer
+def play_recursive_combat(player_1, player_2):
+
+    round = 1
+    # previous_player_1 = []
+    # previous_player_2 = []
+    while len(player_1) > 0 and len(player_2) > 0:
+        # if player_1 == previous_player_1 and player_2 == previous_player_2:
+        #     winner = '1'
+        #     break
+        card_1 = player_1.popleft()
+        card_2 = player_2.popleft()
+
+        print(f'Round {round}: {len(player_1)} >= {card_1} and {len(player_2)} >= {card_2}')
+
+        if len(player_1) >= card_1 and len(player_2) >= card_2:
+            winner, _ = play_recursive_combat(player_1.copy(), player_2.copy())
+        else:
+            winner = '1' if card_1 > card_2 else '2'
+
+        if winner == '1':
+            player_1.append(card_1)
+            player_1.append(card_2)
+        elif winner == '2':
+            player_2.append(card_2)
+            player_2.append(card_1)
+        # previous_player_1 = player_1.copy()
+        # previous_player_2 = player_2.copy()
+        round += 1
+
+    print(player_1)
+    print(player_2)
+    if len(player_2) == 0:
+        winner = player_1
+        winner_name = '1'
+    else:
+        winner_name = '2'
+        winner = player_2
+
+    return winner_name, winner
+
+
+def part1(data: Sequence[str]) -> int:
+    player_1, player_2 = [deque(int(card) for card in player.split()) for player in '\n'.join(data[1:]).split('\n\nPlayer 2:\n')]
+    _, winner = play_combat(player_1, player_2)
 
     return sum((a + 1) * b for a, b in enumerate(reversed(winner)))
 
 
 
 def part2(data: Sequence[str]) -> int:
-    pass
+    player_1, player_2 = [deque(int(card) for card in player.split()) for player in '\n'.join(data[1:]).split('\n\nPlayer 2:\n')]
+    _, winner = play_recursive_combat(player_1, player_2)
+
+    return sum((a + 1) * b for a, b in enumerate(reversed(winner)))
 
 
 def main() -> None:
