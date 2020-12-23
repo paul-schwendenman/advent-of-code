ce26ea276c1eab28f7e64b0f8c894d7db9384109
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-12-23 16:19:32 -0500

    Add tests for part 1

---
+++ 2020/day23/test_day23.py
@@ -0,0 +1,21 @@
+from day23 import part1, part2
+
+
+def test_part1_sample_10_rounds():
+    assert part1('389125467', 10) == '92658374'
+
+
+def test_part1_sample_100_rounds():
+    assert part1('389125467') == '67384529'
+
+
+# def test_part2_sample():
+#     assert part2('389125467') == 291
+
+
+def test_part1_input():
+    assert part1('784235916') == '53248976'
+
+
+# def test_part2_input():
+#     assert part2('') == 31835
