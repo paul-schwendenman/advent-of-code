3f4c1a9cbee6bcf936837ff7dec2b283b6aba136
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-10-08 14:38:09 -0400

    Add day6 part2 solution

--- 2015/day06/day6.py
+++ 2015/day06/day6.py
@@ -28,8 +28,34 @@ def part1(instructions):
     return sum(1 for x in range(0, 1000) for y in range(0, 1000) if grid[(x, y)])
 
 
+def part2(instructions):
+    grid = defaultdict(int)
+
+    parser = re.compile(r'(?P<command>turn on|turn off|toggle) (?P<start_x>[0-9]+),(?P<start_y>[0-9]+) through (?P<end_x>[0-9]+),(?P<end_y>[0-9]+)')
+
+    for instruction in instructions:
+        match = parser.match(instruction).groupdict()
+        command = match['command']
+        start_x = int(match['start_x'])
+        start_y = int(match['start_y'])
+        end_x = int(match['end_x'])
+        end_y = int(match['end_y'])
+
+        for x in range(start_x, end_x + 1):
+            for y in range(start_y, end_y + 1):
+                if command == 'turn on':
+                    grid[(x, y)] += 1
+                elif command == 'turn off':
+                    grid[(x, y)] = max(0, grid[(x, y)] - 1)
+                elif command == 'toggle':
+                    grid[(x, y)] += 2
+
+    return sum(grid[(x, y)] for x in range(0, 1000) for y in range(0, 1000))
+
+
 def main():
-    print(part1(fileinput.input()))
+    # print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
 
 
 if __name__ == '__main__':
--- 2015/day06/test_day6.py
+++ 2015/day06/test_day6.py
@@ -1,5 +1,5 @@
 import pytest
-from day6 import part1
+from day6 import part1, part2
 
 
 @pytest.mark.parametrize("commands,expected_count", [
@@ -7,5 +7,13 @@ from day6 import part1
   (["toggle 0,0 through 999,0"], 1_000),
   (["turn on 0,0 through 999,999", "turn off 499,499 through 500,500"], 1_000_000 - 4),
 ])
-def test_is_nice_string(commands, expected_count):
+def test_part1(commands, expected_count):
     assert expected_count == part1(commands)
+
+
+@pytest.mark.parametrize("commands,expected_count", [
+  (["turn on 0,0 through 0,0"], 1),
+  (["toggle 0,0 through 999,999"], 2000000),
+])
+def test_part2(commands, expected_count):
+    assert expected_count == part2(commands)
