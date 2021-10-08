efddd3d7f612c95aa1526483e33f3872b7fe51ac
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-10-08 17:46:06 -0400

    Add part2

--- 2015/day09/day9.py
+++ 2015/day09/day9.py
@@ -32,20 +32,32 @@ def part1(paths):
         places.add(place_2)
         distances[pair(place_1, place_2)] = int(distance)
 
-    print(distances)
-    print(places)
-
-    # for route in itertools.permutations(places, len(places)):
-    #     route_length = sum(distances[pair(place_1, place_2)] for place_1, place_2 in pairwise(route))
-    #     print(' -> '.join(route) + f" = {route_length}")
-
     return min(sum(distances[pair(place_1, place_2)] for place_1, place_2 in pairwise(route)) for route in itertools.permutations(places, len(places)))
 
 
+def part2(paths):
+    places = set()
+    distances = {}
+    parser = re.compile(r'(?P<place_1>[A-Za-z]+) to (?P<place_2>[A-Za-z]+) = (?P<distance>[0-9]+)')
+
+    for path in paths:
+        matches = parser.match(path).groupdict()
+
+        place_1 = matches['place_1']
+        place_2 = matches['place_2']
+        distance = matches['distance']
+
+        places.add(place_1)
+        places.add(place_2)
+        distances[pair(place_1, place_2)] = int(distance)
+
+    return max(sum(distances[pair(place_1, place_2)] for place_1, place_2 in pairwise(route)) for route in itertools.permutations(places, len(places)))
 
 
 def main():
     print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
+
 
 if __name__ == '__main__':
     main()
