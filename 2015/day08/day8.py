fbe5b926f999393c7ce89a1f83c2d96989977b65
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-10-08 17:11:19 -0400

    Add ugly part2 solution

--- 2015/day08/day8.py
+++ 2015/day08/day8.py
@@ -14,8 +14,27 @@ def part1(strings):
     return code_count - char_count
 
 
+def part2(strings):
+    code_count = 0
+    encode_count = 0
+
+    for string in strings:
+        code_count += len(string.rstrip())
+        encode_count += len(string.rstrip().replace('\\', '\\\\').replace('"', '\\"')) + 2
+        # encode_count += 2 + code_count + string.count('/') + string.count('"')
+        # new = string.rstrip().replace('\\', '\\\\').replace('"', '\\"')
+        # print(f"""{string}: {new}""")
+        # print(f"""{len(new)} - {len(string.rstrip())}""")
+
+        pass
+
+    print(f"{encode_count} - {code_count}")
+    return encode_count - code_count
+
+
 def main():
     print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
 
 
 if __name__ == '__main__':
