6875093813d99b8430147f1d8092b76b4f9536a4
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-10-14 23:41:34 -0400

    Add part 1 solution

--- 2015/day17/day17.py
+++ 2015/day17/day17.py
@@ -1,8 +1,19 @@
 import fileinput
 
 
+def distribute(remainder, containers):
+	if remainder == 0:
+		return 1
+	elif len(containers) == 0 or remainder < 0:
+		return 0
+
+	return sum(distribute(remainder - item, containers[(index + 1):]) for index, item in enumerate(containers))
+
+
 def part1(goal, containers):
-	pass
+	containers = sorted(map(int, containers), reverse=True)
+
+	return distribute(goal, containers)
 
 
 def main():
