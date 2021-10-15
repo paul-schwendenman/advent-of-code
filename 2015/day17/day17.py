0d49ff2eb270d1a0d70b333723bab70c8b937abd
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-10-14 20:31:40 -0400

    Add part1 skeleton

---
+++ 2015/day17/day17.py
@@ -0,0 +1,13 @@
+import fileinput
+
+
+def part1(goal, containers):
+	pass
+
+
+def main():
+	print(part1(150, fileinput.input()))
+
+
+if __name__ == '__main__':
+	main()
---
+++ 2015/day17/test_day17.py
@@ -0,0 +1,5 @@
+from day17 import part1
+
+
+def test_part1():
+	assert 4 == part1(25, [20, 15, 10, 5, 5])
