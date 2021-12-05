1b6cbb3c493598140d82d30fafa3b8d732796edc
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-05 00:55:15 -0500

    Add tests for day05

---
+++ 2021/test_day05.py
@@ -0,0 +1,16 @@
+from day05 import line_to_array
+
+def test_vertical_line():
+    assert list(line_to_array('1,1 -> 1,3')) == [(1, 1), (1, 2), (1, 3)]
+
+def test_horizontal_line():
+    assert list(line_to_array('9,7 -> 7,7')) == [(7, 7), (8, 7), (9, 7)]
+
+def test_ignores_diagonal_line():
+    assert list(line_to_array('1,1 -> 3,3')) == []
+
+def test_diagonal_line():
+    assert list(line_to_array('1,1 -> 3,3', include_diagonals=True)) == [(1, 1), (2, 2), (3, 3)]
+
+def test_other_diagonal_line():
+    assert list(line_to_array('9,7 -> 7,9', include_diagonals=True)) == [(9, 7), (8, 8), (7, 9)]
