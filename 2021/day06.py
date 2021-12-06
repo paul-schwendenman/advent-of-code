a8bf763005eb5e5683e79a01b49c6e7af529cbde
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-06 01:10:08 -0500

    Refactor day06

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,70 +1,17 @@
 import fileinput
 from collections import Counter
 
-class Fish:
-    def __init__(self, age=8):
-        self._age = age
-        self._children = []
 
-    def age(self):
-        self._age -= 1
-
-        if self._age < 0:
-            self._children.append(Fish())
-            self._age = 6
-
-    def children(self):
-        self._children, out = [], self._children
-
-        return out
-
-class FishTwo:
-    def __init__(self, age=8, count=1):
-        self._age = age
-        self._children = []
-
-    def age(self):
-        self._age -= 1
-
-        if self._age < 0:
-            self._children.append(Fish())
-            self._age = 6
-
-    def children(self):
-        self._children, out = [], self._children
-
-        return out
-
-
-def part1(fish):
-    fish = list(map(Fish, fish))
-
-    for i in range(80):
-        print(f'day {i}: {len(fish)} fish')
-        new = []
-        for a_fish in fish:
-            a_fish.age()
-            new.extend(a_fish.children())
-
-        fish.extend(new)
-
-    return len(fish)
-    pass
-
-def part2(fish):
+def solve(fish, days=80):
     fish = dict(Counter(fish))
 
-    for i in range(256):
-        # print(f'day {i}: {sum(fish.values())} fish')
+    for _ in range(days):
         spawn_count = fish.get(0, 0)
-        fish[0] = fish.get(1, 0)
-        fish[1] = fish.get(2, 0)
-        fish[2] = fish.get(3, 0)
-        fish[3] = fish.get(4, 0)
-        fish[4] = fish.get(5, 0)
-        fish[5] = fish.get(6, 0)
-        fish[6] = fish.get(7, 0) + spawn_count
-        fish[7] = fish.get(8, 0)
+
+        for age in range(8):
+            fish[age] = fish.get(age + 1, 0)
+
+        fish[6] += spawn_count
         fish[8] = spawn_count
 
     return sum(fish.values())
@@ -72,10 +19,11 @@ def part2(fish):
 
 def main():
     with fileinput.input() as input:
-        fish = [int(age) for age in list(input)[0].split(',')]
+        fish = [int(age) for age in list(input)[0].split(",")]
 
-    # print(part1(fish))
-    print(part2(fish))
+    print(solve(fish))
+    print(solve(fish, 256))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
