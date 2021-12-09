9fd7c4295a43e86ab5f51e77718235ad20bee20c
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-09 01:20:23 -0500

    Refactor solutions

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,5 +1,9 @@
 import fileinput
-from functools import reduce, lru_cache
+
+
+def parse_grid(lines):
+    return {(x, y): h for y, row in enumerate(lines) for x, h in enumerate(row)}
+
 
 def get_neighboors(x, y):
     for dx in (-1, 1):
@@ -8,26 +12,21 @@ def get_neighboors(x, y):
         yield (x, y + dy)
 
 
-def part1(lines):
-    grid = {}
+def find_low_points(grid):
     low_points = []
-    # parse_grid()
 
-    for y, row in enumerate(lines):
-        for x, location in enumerate(row):
-            grid[(x,y)] = location
+    for location, height in grid.items():
+        if all(
+            grid.get(neighboor, 10) > height for neighboor in get_neighboors(*location)
+        ):
+            low_points.append((location, height))
 
-    print(grid)
-
-    for y, row in enumerate(lines):
-        for x, location in enumerate(row):
-            # print((x, y), location, [grid.get(neighboor, 0) < location for neighboor in get_neighboors(x, y)])
-            if all(grid.get(neighboor, 1000) > location for neighboor in get_neighboors(x, y)):
-                # print((x, y), location, [(grid.get(neighboor, 1000) > location, grid.get(neighboor), neighboor) for neighboor in get_neighboors(x, y)])
-                low_points.append(location)
+    return low_points
 
 
-    # low_points = find_low_points()
+def part1(lines):
+    grid = parse_grid(lines)
+    low_points = [height for _, height in find_low_points(grid)]
 
     return sum(low_points) + len(low_points)
 
@@ -44,27 +43,12 @@ def find_basin(grid, basin):
 
 
 def part2(lines):
-    grid = {}
-    low_points = []
-    # parse_grid()
+    grid = parse_grid(lines)
+    low_points = [location for location, _ in find_low_points(grid)]
 
-    for y, row in enumerate(lines):
-        for x, location in enumerate(row):
-            grid[(x,y)] = location
-
-    # print(grid)
-
-    for y, row in enumerate(lines):
-        for x, location in enumerate(row):
-            if all(grid.get(neighboor, 1000) > location for neighboor in get_neighboors(x, y)):
-                low_points.append((x, y))
-
-
-    # low_points = find_low_points()
     basins = [find_basin(grid, [low_point]) for low_point in low_points]
 
     sizes = list(sorted(map(len, basins)))
-    print(sizes)
 
     return sizes[-1] * sizes[-2] * sizes[-3]
 
@@ -73,8 +57,9 @@ def main():
     with fileinput.input() as input:
         lines = [[int(location) for location in line.rstrip()] for line in input]
 
-    # print(part1(lines))
+    print(part1(lines))
     print(part2(lines))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
