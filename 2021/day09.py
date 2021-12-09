95ccd37a9aac40c66911c06cf98c23dcd0255630
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-09 00:53:53 -0500

    Add slow part2

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,4 +1,5 @@
 import fileinput
+from functools import reduce, lru_cache
 
 def get_neighboors(x, y):
     for dx in (-1, 1):
@@ -22,7 +23,7 @@ def part1(lines):
         for x, location in enumerate(row):
             # print((x, y), location, [grid.get(neighboor, 0) < location for neighboor in get_neighboors(x, y)])
             if all(grid.get(neighboor, 1000) > location for neighboor in get_neighboors(x, y)):
-                print((x, y), location, [(grid.get(neighboor, 1000) > location, grid.get(neighboor), neighboor) for neighboor in get_neighboors(x, y)])
+                # print((x, y), location, [(grid.get(neighboor, 1000) > location, grid.get(neighboor), neighboor) for neighboor in get_neighboors(x, y)])
                 low_points.append(location)
 
 
@@ -31,11 +32,50 @@ def part1(lines):
     return sum(low_points) + len(low_points)
 
 
+@lru_cache(maxsize=10_000_000)
+def find_basin(location, grid, path=[]):
+    if location not in grid:
+        return set()
+    if grid[location] == 9:
+        return set()
+    if location in path:
+        return set()
+
+    return set((location,)) | reduce(lambda a, b: a | b, (find_basin(neighboor, grid, path+[location]) for neighboor in get_neighboors(*location)))
+
+
+def part2(lines):
+    grid = {}
+    low_points = []
+    # parse_grid()
+
+    for y, row in enumerate(lines):
+        for x, location in enumerate(row):
+            grid[(x,y)] = location
+
+    # print(grid)
+
+    for y, row in enumerate(lines):
+        for x, location in enumerate(row):
+            if all(grid.get(neighboor, 1000) > location for neighboor in get_neighboors(x, y)):
+                low_points.append((x, y))
+
+
+    # low_points = find_low_points()
+    basins = [find_basin(low_point) for low_point in low_points]
+
+    sizes = list(sorted(map(len, basins)))
+    print(sizes)
+
+    return sizes[-1] * sizes[-2] * sizes[-3]
+
+
 def main():
     with fileinput.input() as input:
         lines = [[int(location) for location in line.rstrip()] for line in input]
 
-    print(part1(lines))
+    # print(part1(lines))
+    print(part2(lines))
 
 if __name__ == '__main__':
     main()
