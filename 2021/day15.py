24c4e3e21835c3ad3fffe7edb3f9b8ddf0a8d141
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-15 01:12:50 -0500

    Refactor day15 a bit

--- 2021/day15.py
+++ 2021/day15.py
@@ -3,54 +3,58 @@ from collections import *
 
 
 def neighboors(location):
-	x, y = location
+    x, y = location
 
-	for dx in (1, -1):
-		yield (x + dx, y)
-	for dy in (-1, 1):
-		yield (x, y + dy)
+    for dx in (1, -1):
+        yield (x + dx, y)
+    for dy in (-1, 1):
+        yield (x, y + dy)
+
+
+def find_cost(grid, start, goal):
+    checklist = [(0, start)]
+
+    costs = {}
+    cost = None
+
+    while True:
+        cost, location = checklist[0]
+
+        if location == goal:
+            return cost
+
+        checklist = checklist[1:]
+
+        for neighboor in neighboors(location):
+            if neighboor not in grid:
+                continue
+
+            new_cost = cost + grid[neighboor]
+
+            if neighboor in costs and costs[neighboor] <= new_cost:
+                continue
+
+            costs[neighboor] = new_cost
+            checklist.append((new_cost, neighboor))
+
+        checklist = sorted(checklist)
 
 
 def part1(lines):
-	grid = {(x, y): int(value) for y, line in enumerate(lines) for x, value in enumerate(line)}
+    grid = {
+        (x, y): int(value)
+        for y, line in enumerate(lines)
+        for x, value in enumerate(line)
+    }
 
-	start = (0, 0)
-	end = (len(lines[0]) - 1, len(lines) - 1)
+    start = (0, 0)
+    end = (len(lines[0]) - 1, len(lines) - 1)
 
-	checklist = [(0, start)]
-
-	costs = {}
-	cost = None
-
-	while True:
-		cost, location = checklist[0]
-
-		if location == end:
-			break
-
-		checklist = checklist[1:]
-
-		for neighboor in neighboors(location):
-			if neighboor not in grid:
-				continue
-
-			new_cost = cost + grid[neighboor]
-
-			if neighboor in costs and costs[neighboor] <= new_cost:
-				continue
-
-			costs[neighboor] = new_cost
-			checklist.append((new_cost, neighboor))
-
-		checklist = sorted(checklist)
-
-
-	return cost
-
+    return find_cost(grid, start, end)
 
 
 def part2(lines):
-	pass
+    pass
 
 
 def main():
