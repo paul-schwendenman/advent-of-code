0b8abf311b8314d536a939d9688930b3c6243e1f
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-17 00:54:57 -0500

    Refactor day17

--- 2021/day17.py
+++ 2021/day17.py
@@ -1,11 +1,32 @@
 import fileinput
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import Optional, Tuple
 
 
-def fire(dx, dy, x_min, x_max, y_min, y_max):
+@dataclass
+class TargetArea:
+    x_min: int
+    x_max: int
+    y_min: int
+    y_max: int
+    pass
+
+
+def parse_target_area(raw: str) -> TargetArea:
+    target_area = raw[13:].split(", ")
+
+    [x_min, x_max] = [int(item) for item in target_area[0][2:].split("..")]
+    [y_min, y_max] = [int(item) for item in target_area[1][2:].split("..")]
+
+    return TargetArea(x_min, x_max, y_min, y_max)
+
+
+def fire(dx: int, dy: int, target_area: TargetArea) -> Tuple[bool, Optional[int]]:
     highest_point = -1_000_000_000
     x, y = (0, 0)
 
-    while x < x_max and y > y_min:
+    while x < target_area.x_max and y > target_area.y_min:
         x += dx
         y += dy
 
@@ -15,28 +36,28 @@ def fire(dx, dy, x_min, x_max, y_min, y_max):
         if highest_point < y:
             highest_point = y
 
-        if x_min <= x <= x_max and y_min <= y <= y_max:
+        if (
+            target_area.x_min <= x <= target_area.x_max
+            and target_area.y_min <= y <= target_area.y_max
+        ):
             return True, highest_point
 
     return False, None
 
 
 def part1(data):
-    target_area = data[0][13:].split(", ")
+    target_area = parse_target_area(data[0])
     highest_point = -1_000_000_000
 
-    [x_min, x_max] = [int(item) for item in target_area[0][2:].split("..")]
-    [y_min, y_max] = [int(item) for item in target_area[1][2:].split("..")]
-
-    target_velocity = None
+    # target_velocity = None
 
     for dy in range(-1000, 1000):
         for dx in range(100):
-            hit, new_highest_point = fire(dx, dy, x_min, x_max, y_min, y_max)
+            hit, new_highest_point = fire(dx, dy, target_area)
 
             if hit and new_highest_point > highest_point:
                 highest_point = new_highest_point
-                target_velocity = dx, dy
+                # target_velocity = dx, dy
 
     # print(f"target velocity: {target_velocity}")
     # print(f"highest point: {highest_point}")
@@ -45,16 +66,12 @@ def part1(data):
 
 
 def part2(data):
-    target_area = data[0][13:].split(", ")
-    highest_point = -1_000_000_000
+    target_area = parse_target_area(data[0])
     velocities = []
 
-    [x_min, x_max] = [int(item) for item in target_area[0][2:].split("..")]
-    [y_min, y_max] = [int(item) for item in target_area[1][2:].split("..")]
-
-    for dy in range(-1000, 3000):
+    for dy in range(-1000, 2000):
         for dx in range(1000):
-            hit, _ = fire(dx, dy, x_min, x_max, y_min, y_max)
+            hit, _ = fire(dx, dy, target_area)
 
             if hit:
                 velocities.append((dx, dy))
