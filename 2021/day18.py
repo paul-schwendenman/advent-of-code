8b20b300ec1d7c4d06fe58a7c9d5afa8d23bd40e
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-18 00:58:49 -0500

    Refactor to remove dataclass

--- 2021/day18.py
+++ 2021/day18.py
@@ -1,60 +1,38 @@
 import fileinput
 from dataclasses import dataclass
 
-@dataclass
-class SnailNumber:
-    x: int
-    y: int
-
-    def __str__(self):
-        return f'[{self.x},{self.y}]'
 
 def parse(string):
-    print(string)
-    raw = eval(string)
-
-    def inner(lst):
-        [raw_x, raw_y] = lst
-
-        try:
-            x = int(raw_x)
-        except:
-            x = inner(raw_x)
-        try:
-            y = int(raw_y)
-        except:
-            y = inner(raw_y)
-
-        return SnailNumber(x, y)
-
-    return inner(raw)
+    return eval(string)
 
 
 def reduce(number):
     pass
 
+def expode(number):
+    pass
+
+
+def split(number: int):
+    pass
+
 
 def add(number_1, number_2):
-    return SnailNumber(x=number_1, y=number_2)
+    return [number_1, number_2]
 
 
-def calc_magnitude(number):
+def magnitude(number):
     if isinstance(number, int):
         return number
 
-    x, y = number.x, number.y
+    [x, y] = number
 
-    if isinstance(x, SnailNumber):
-        x = calc_magnitude(x)
-
-    if isinstance(y, SnailNumber):
-        y = calc_magnitude(y)
-
-    print(f'{x} * 3 + {y} * 2 = {x * 3 + y * 2}')
-    return x * 3 + y * 2
+    return 3 * magnitude(x) + 2 * magnitude(y)
 
 
 def part1(data):
+
+    numbers = [parse(item) for item in data]
     pass
 
 
