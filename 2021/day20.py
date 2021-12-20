155c4cd77ee92a05d44ea11b551f003e132c8932
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-20 01:32:43 -0500

    Add part2

--- 2021/day20.py
+++ 2021/day20.py
@@ -1,10 +1,4 @@
 import fileinput
-from collections import defaultdict
-
-
-def print_grid(grid):
-    for j in range(-10, 110):
-        print("".join("#" if grid.get((i, j), 0) == 1 else '.' for i in range(-10, 110)))
 
 
 def neighboors(pixel):
@@ -15,58 +9,61 @@ def neighboors(pixel):
             yield (x + i, y + j)
 
 
-def part1(data):
-    image_enhancement_algorithm, raw_image = data[0], data[2:]
+def parse_image(raw_image):
+    # image = {}
 
-    image = defaultdict(int)
+    # for j, row in enumerate(raw_image):
+    #     for i, value in enumerate(row):
+    #         image[(i, j)] = 1 if value == "#" else 0
 
-    for j, row in enumerate(raw_image):
-        for i, value in enumerate(row):
-            image[(i, j)] = 1 if value == '#' else 0
+    return {
+        (i, j): (1 if value == "#" else 0)
+        for j, row in enumerate(raw_image)
+        for i, value in enumerate(row)
+    }
 
 
-    pixels = [(i, j) for j in range(-100, len(raw_image) + 100) for i in range(-100, len(raw_image[0]) + 100)]
+def process_image(image_enhancement_algorithm, pixels, step):
+    rows = []
+    columns = []
 
-    round_1 = {}
+    for x, y in pixels:
+        rows.append(y)
+        columns.append(x)
 
-    for pixel in pixels:
-        # if all(neighboor not in image for neighboor in neighboors(pixel)):
-        #     continue
-        try:
-            code = int("".join(str(image[neighboor]) for neighboor in neighboors(pixel)), 2)
-        except Exception as e:
-            print([str(image[neighboor]) for neighboor in neighboors(pixel)])
-            # code = int("".join(str(image[neighboor]) for neighboor in neighboors(pixel)), 2)
-            raise e
-        round_1[pixel] = 1 if image_enhancement_algorithm[code] == '#' else 0
-        pass
-
-    # print_grid(round_1)
-    # print('-------------')
+    max_x = max(rows)
+    min_x = min(rows)
+    max_y = max(columns)
+    min_y = min(columns)
 
     output = {}
 
-    for pixel in pixels:
-        # if all(neighboor not in round_1 for neighboor in neighboors(pixel)):
-        #     continue
-        try:
-            code = int("".join(str(round_1.get(neighboor, 1)) for neighboor in neighboors(pixel)), 2)
-        except Exception as e:
-            print(pixel)
-            print(list(neighboors(pixel)))
-            print([str(output.get(neighboor, 0)) for neighboor in neighboors(pixel)])
-            # code = int("".join(str(image[neighboor]) for neighboor in neighboors(pixel)), 2)
-            raise e
+    for j in range(min_y - 2, max_y + 2):
+        for i in range(min_x - 2, max_x + 2):
+            pixel = (i, j)
 
-        output[pixel] = 1 if image_enhancement_algorithm[code] == '#' else 0
+            code = int(
+                "".join(
+                    str(pixels.get(neighboor, step % 2))
+                    for neighboor in neighboors(pixel)
+                ),
+                2,
+            )
 
-    # print_grid(output)
+            output[pixel] = 1 if image_enhancement_algorithm[code] == "#" else 0
 
-    return sum(1 for key, item in output.items() if item and abs(key[0] < 150) and abs(key[1]) < 150)
+    return output
 
 
-def part2(data):
-    pass
+def part1(data, steps=2):
+    image_enhancement_algorithm, raw_image = data[0], data[2:]
+
+    image = parse_image(raw_image)
+
+    for step in range(steps):
+        image = process_image(image_enhancement_algorithm, image, step)
+
+    return sum(1 for item in image.values() if item)
 
 
 def main():
@@ -74,7 +71,7 @@ def main():
         data = [line.rstrip() for line in input]
 
     print(part1(data))
-    print(part2(data))
+    print(part1(data, 50))
 
 
 if __name__ == "__main__":
