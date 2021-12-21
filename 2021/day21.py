6661f963e081066225597121656e00adca2b7560
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-12-21 01:04:53 -0500

    Add part2

--- 2021/day21.py
+++ 2021/day21.py
@@ -1,5 +1,16 @@
 import fileinput
-from itertools import repeat, cycle
+from itertools import repeat, cycle, product
+from collections import Counter, namedtuple
+from typing import Mapping
+from enum import Enum
+
+
+State = namedtuple('State', 'player_1_location player_1_score player_2_location player_2_score current_player')
+
+
+class Player(Enum):
+	PLAYER_1 = 1
+	PLAYER_2 = 2
 
 
 def part1(data: list[str]) -> int:
@@ -52,7 +63,51 @@ def part1(data: list[str]) -> int:
 
 
 def part2(data: list[str]) -> int:
-	pass
+	player_1_location = int(data[0][28:])
+	player_2_location = int(data[1][28:])
+	player_1_score = 0
+	player_2_score = 0
+	initial_player = Player.PLAYER_1
+
+	games: Mapping[State, int] = Counter()
+	done: Mapping[int, int] = Counter()
+
+	games[State(player_1_location, player_1_score, player_2_location, player_2_score, initial_player)] += 1
+
+	while len(games):
+		print(f'games: {len(games)}')
+		new_games = Counter()
+
+		for game, quantity in games.items():
+			if game.current_player == Player.PLAYER_1:
+				all_rolls = product(range(1, 4), range(1, 4), range(1, 4))
+				for rolls in all_rolls:
+					player_1_location = ((sum(rolls) + game.player_1_location - 1) % 10) + 1
+
+					player_1_score = game.player_1_score + player_1_location
+
+					if player_1_score >= 21:
+						done[1] += quantity
+					else:
+						state = State(player_1_location, player_1_score, game.player_2_location, game.player_2_score, Player.PLAYER_2)
+						new_games[state] += quantity
+			else:
+				all_rolls = product(range(1, 4), range(1, 4), range(1, 4))
+				for rolls in all_rolls:
+					player_2_location = ((sum(rolls) + game.player_2_location - 1) % 10) + 1
+
+					player_2_score = game.player_2_score + player_2_location
+
+					if player_2_score >= 21:
+						done[2] += quantity
+					else:
+						state = State(game.player_1_location, game.player_1_score, player_2_location, player_2_score, Player.PLAYER_1)
+						new_games[state] += quantity
+
+		games = new_games
+
+	print(done)
+	return done.most_common()[0][1]
 
 
 def main():
