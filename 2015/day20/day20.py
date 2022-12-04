7b01496bfccbc158699592d886f09d588dc7a119
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-03 23:56:04 -0500

    Add day 20 part 2 solution

--- 2015/day20/day20.py
+++ 2015/day20/day20.py
@@ -14,9 +14,24 @@ def part1(goal, gifts_per_elf = 10):
 			return house
 
 
+def part2(goal, gifts_per_elf = 11):
+	houses = defaultdict(int)
+
+	for elf in range(1, (goal // gifts_per_elf) + 1):
+		for house in range(elf, min(elf * 51, goal // gifts_per_elf), elf):
+			houses[house] += elf
+
+	for house, score in houses.items():
+		if score * gifts_per_elf > goal:
+			return house
+
+
 def main():
 	# print(part1(100))
-	print(part1(36000000))
+	# print(part1(36000000))
+	print(part2(36000000))
+	# print(part1(29000000))
+	# print(part2(29000000))
 
 
 if __name__ == '__main__':
