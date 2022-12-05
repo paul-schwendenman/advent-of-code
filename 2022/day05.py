6fb9424cc23b2d84afc2646f3cd9a42fe40f63aa
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-05 00:32:32 -0500

    Remove debugging statements

--- 2022/day05.py
+++ 2022/day05.py
@@ -15,16 +15,12 @@ def part1(data):
 		8: list('DNQMTJ'),
 		9: list('PGS'),
 	}
-	print(stacks)
 
 	for instruction in instructions[:]:
 		_, count, _, fromm, _, to = instruction.split(' ')
 
-		print(instruction, count, fromm, to)
 		for i in range(int(count)):
 			stacks[int(to)].append(stacks[int(fromm)].pop())
-
-	print(stacks)
 
 	output = []
 
@@ -51,30 +47,25 @@ def part2(data):
 		8: list('DNQMTJ'),
 		9: list('PGS'),
 	}
-	print(stacks)
 
 	for instruction in instructions[:]:
 		_, count, _, fromm, _, to = instruction.split(' ')
 
-		print(instruction, count, fromm, to)
 		substack = []
 		for i in range(int(count)):
 			substack.append(stacks[int(fromm)].pop())
 
 		stacks[int(to)].extend(reversed(substack))
 
-	print(stacks)
-
 	output = []
 
-	for num, stack in sorted(stacks.items()):
+	for _, stack in sorted(stacks.items()):
 		if len(stack):
 			output += stack[len(stack) - 1]
 		else:
 			output += ' '
 
 	return ''.join(output)
-	pass
 
 
 def main():
