1bdc8c3d73dbb60902052cba6bdf88af167daf7e
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-06 00:28:15 -0500

    Refactor a bit

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,33 +1,23 @@
 import fileinput
 
 
-def part1(data):
+def part1(data, size = 4):
 	line = list(data)[0]
 
-	for index, char in enumerate(line[3:]):
-		# print(set(line[index:index+4]))
-		if len(set(line[index:index+4])) < 4:
-			continue
-		return index + 4
-	pass
+	for index, _ in enumerate(line):
+		if len(set(line[index-size:index])) == size:
+			return index
 
 
 def part2(data):
-	line = list(data)[0]
-
-	for index, char in enumerate(line[13:]):
-		# print(set(line[index:index+14]))
-		if len(set(line[index:index+14])) < 14:
-			continue
-		return index + 14
-	pass
+	return part1(data, size=14)
 
 
 def main():
 	print(part1(fileinput.input()))
-	assert(part1(fileinput.input()) == 1912)
+	# assert(part1(fileinput.input()) == 1912)
 	print(part2(fileinput.input()))
-	assert(part2(fileinput.input()) == 2122)
+	# assert(part2(fileinput.input()) == 2122)
 
 
 if __name__ == '__main__':
