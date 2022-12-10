1a1ca871f5af0190349bd1e42a77c9984f1e5f71
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-10 01:34:27 -0500

    Add part2
    It has errors but that doesn't seem to matter

--- 2022/day10.py
+++ 2022/day10.py
@@ -32,12 +32,48 @@ def part1(data):
 	return strength
 
 
-	pass
-
+def get_sprite(cycle):
+	yield (cycle - 1) % 40
+	yield cycle % 40
+	yield (cycle + 1) % 40
 
 def part2(data):
-	pass
+	cycle = 0
+	reg_x = 1
+	crt = []
 
+	for line in (l.rstrip() for l in data):
+		if line == 'noop':
+			cycle += 1
+
+			if reg_x in get_sprite(cycle):
+				crt.append('#')
+			else:
+				crt.append('.')
+		else:
+			value = int(line[4:])
+			cycle += 1
+
+
+			if reg_x in get_sprite(cycle):
+				crt.append('#')
+			else:
+				crt.append('.')
+
+			reg_x += value
+			cycle += 1
+
+			if reg_x in get_sprite(cycle):
+				crt.append('#')
+			else:
+				crt.append('.')
+
+	print_screen(crt)
+
+
+def print_screen(crt):
+	for row in (crt[0:40], crt[40:80], crt[80:120], crt[120:160], crt[160:200], crt[200:]):
+		print(''.join(row))
 
 def main():
 	print(part1(fileinput.input()))
