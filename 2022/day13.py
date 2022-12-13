78ccc4730f09f7a24d7014fd23b251cb0bff3cdc
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-13 00:35:34 -0500

    Add part1

---
+++ 2022/day13.py
@@ -0,0 +1,60 @@
+import fileinput
+
+
+def compare(a, b):
+    types = type(a), type(b)
+    # print(a, b, types)
+
+    if types == (int, int):
+        if a < b:
+            return True
+        elif a > b:
+            return False
+        return None
+    elif types == (list, int):
+        return compare(a, [b])
+    elif types == (int, list):
+        return compare([a], b)
+    elif types == (list, list):
+        # print('sublist:', len(a), len(b), max(len(a), len(b)))
+        for i in range(max(len(a), len(b))):
+            if len(a) <= i:
+                return True
+            elif len(b) <= i:
+                return False
+            else:
+                sub = compare(a[i], b[i])
+
+                if sub is None:
+                    continue
+                return sub
+    else:
+        raise ValueError("Types: %s", types)
+
+
+def part1(data):
+    pairs = (tuple(eval(pairing.strip()) for pairing in pair.split('\n')) for pair in ''.join(data).strip().split('\n\n'))
+    count = 0
+    correct_pairs = []
+
+    for i, pair in enumerate(pairs):
+        if result := compare(*pair):
+            count += 1
+            correct_pairs.append(i)
+        print(i, pair, result)
+
+    return sum(correct_pairs) + len(correct_pairs)
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
+
+
+if __name__ == '__main__':
+    main()
