16dca8c81126eb5fe7c6e86e0bd4c09c5e14ce24
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-17 23:53:14 -0500

    Add helpers

--- 2022/helper.py
+++ 2022/helper.py
@@ -1,6 +1,24 @@
 import re
-from collections.abc import Sequence
+from collections.abc import *
+from collections import *
+from functools import *
+from itertools import *
+from math import inf, prod
+from enum import Enum
+from dataclasses import dataclass, field
 
 
 def extract_ints(line: str) -> Sequence[int]:
     return list(map(int, re.findall(r'-?[0-9]+', line)))
+
+
+class Point(namedtuple('Point', 'x y')):
+    __slots__ = ()
+
+    def __add__(self, other):
+        return Point(self.x + other[0], self.y + other[1])
+
+
+
+def calc_manhatten_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
