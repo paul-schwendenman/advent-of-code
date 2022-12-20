7d46666b0b1ffb24350f728fbf245a93869959a2
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-20 01:44:12 -0500

    Add part2

--- 2022/day20.py
+++ 2022/day20.py
@@ -11,7 +11,8 @@ def parse_all_ints(s):
     return list(map(int, s.split()))
 
 
-def part1(data):
+def part1(data, decryption_key=1):
+
     sequence = tuple(extract_ints(line)[0] for line in data)
     working = deque(map(Wrapper, sequence))
 
@@ -52,14 +53,33 @@ def part1(data):
 
     coords = [answer[(offset + n) % len(answer)] for n in (1000, 2000, 3000)]
 
-    print(f'{coords=}')
+    return sum(coords)
+
+
+def part2(data, decryption_key=811589153):
+
+    sequence = tuple(map(Wrapper, (extract_ints(line)[0] * decryption_key for line in data)))
+    working = deque(sequence)
+
+
+    for _ in range(10):
+        for item in tuple(sequence):
+            item_index = working.index(item)
+
+            working.rotate(-item_index)
+
+            piece = working.popleft()
+
+            working.rotate(-item.number)
+
+            working.appendleft(item)
+
+    answer = [item.number for item in working]
+    offset = answer.index(0)
+
+    coords = [answer[(offset + n) % len(answer)] for n in (1000, 2000, 3000)]
 
     return sum(coords)
-    pass
-
-
-def part2(data):
-    pass
 
 
 def main():
