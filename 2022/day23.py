29096fe4db8b711bc14e560b916c77ece5660659
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-23 02:40:43 -0500

    Add part2 (still running for input)

--- 2022/day23.py
+++ 2022/day23.py
@@ -133,11 +133,90 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    grid = (parse_input(data))
+    total_elves = len(grid)
+
+    print(f'{total_elves}') #grid)
+
+    print_grid(grid)
+
+    directions = deque(['N', 'S', 'W', 'E'])
+
+    for i in tqdm(count()):
+        next_locations = defaultdict(list)
+        new_grid = {}
+
+        for elf in grid.keys():
+            has_neighbors_n = (set(elf.neighbors_n()) & set(grid.keys()))
+            has_neighbors_s = (set(elf.neighbors_s()) & set(grid.keys()))
+            has_neighbors_w = (set(elf.neighbors_w()) & set(grid.keys()))
+            has_neighbors_e = (set(elf.neighbors_e()) & set(grid.keys()))
+
+            # print(f'{elf=} {has_neighbors_n=} {has_neighbors_s=} {has_neighbors_w=} {has_neighbors_e=}')
+
+            if not any([has_neighbors_n, has_neighbors_s, has_neighbors_w, has_neighbors_e]):
+                next_locations[elf].append(elf)
+            else:
+                for direction in directions:
+                    # print(f'testing {direction}')
+                    if direction == 'N' and not has_neighbors_n:
+                        # print('trying north')
+                        next_locations[elf + (0, -1)].append(elf)
+                        break
+                    elif direction == 'S' and not has_neighbors_s:
+                        # print('trying south')
+                        next_locations[elf + (0, 1)].append(elf)
+                        break
+                    elif direction == 'W' and not has_neighbors_w:
+                        # print('trying west')
+                        next_locations[elf + (-1, 0)].append(elf)
+                        break
+                    elif direction == 'E' and not has_neighbors_e:
+                        # print('trying east')
+                        next_locations[elf + (1, 0)].append(elf)
+                        break
+                else:
+                    next_locations[elf].append(elf)
+                    # raise ValueError
+
+            # print(len(next_locations))
+
+        for location, elves in next_locations.items():
+            if len(elves) == 1:
+                new_grid[location] = True
+            else:
+                # print(f'multiple elves: {location} {elves}')
+                for elf in elves:
+                    new_grid[elf] = True
+
+        directions.rotate(-1)
+        # print(f'{len(new_grid)=}') #, new_grid)
+        if set(grid.keys()) == set(new_grid.keys()):
+            break
+            # return i + 1
+        grid = new_grid
+
+        # print_grid(grid)
+        assert len(new_grid) == total_elves
+
+        # input()
+
+    # min_x = min(point.x for point in grid)
+    # max_x = max(point.x for point in grid)
+    # min_y = min(point.y for point in grid)
+    # max_y = max(point.y for point in grid)
+
+    # area = (1 + max_x - min_x) * (1 + max_y - min_y)
+
+    # print(f'{min_x=} {max_y=} {min_x=} {max_x=} {area=}')
+    # print(sum(1 for point, value in grid.items() if value))
+
+    print()
+    return i + 1
 
 
 def main():
-    print(part1(fileinput.input()))
+    # print(part1(fileinput.input()))
     print(part2(fileinput.input()))
 
 
