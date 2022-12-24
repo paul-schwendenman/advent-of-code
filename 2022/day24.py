9a5330b0982c442dd72d97186b9d4ed6b44a669e
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-12-24 11:37:56 -0500

    Add replay, fix start location, record path

--- 2022/day24.py
+++ 2022/day24.py
@@ -3,12 +3,16 @@ from helper import *
 from heapq import heapify, heappop, heappush
 
 
-class State(namedtuple('State', 'score time location blizzards')):
+class State(namedtuple('State', 'score time path blizzards')):
     __slots__ = ()
 
     @classmethod
-    def build(cls, location, blizzards, goal, time):
-        return cls(calc_manhatten_distance(goal, location) + time, time, location, blizzards)
+    def build(cls, path, blizzards, goal, time):
+        return cls(calc_manhatten_distance(goal, path[-1]) + time, time, path, blizzards)
+
+    @property
+    def location(self):
+        return self.path[-1]
 
 def moves(point):
     for diff in [(0, 0), (1, 0), (-1, 0), (0, -1), (0, 1)]:
@@ -62,9 +66,41 @@ def cache_blizzards(blizzards):
     return tuple((key, tuple(value)) for key, value in blizzards.items())
 
 
+def replay(grid, initial_blizzards, path):
+    blizzard = initial_blizzards
+    max_x = max((point.x for point in grid.keys()))
+    min_x = min((point.x for point in grid.keys()))
+    max_y = max((point.y for point in grid.keys()))
+    min_y = min((point.y for point in grid.keys()))
+
+    for location in path:
+        print(f'---- {location} ----')
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                if (x, y) in grid and grid[(x, y)] == '#':
+                    if (x,y) == location:
+                        raise ValueError
+                    print('#', end='')
+                elif Point(x, y) == location:
+                    print('E', end='')
+                elif (x,y) in blizzard['>']:
+                    print('>', end='')
+                elif (x,y) in blizzard['<']:
+                    print('<', end='')
+                elif (x,y) in blizzard['^']:
+                    print('^', end='')
+                elif (x,y) in blizzard['v']:
+                    print('v', end='')
+                else:
+                    print('.', end='')
+            print()
+        cachable_blizzards = cache_blizzards(blizzard)
+        blizzard = move_blizzards(cachable_blizzards, min_x=min_x, max_x=max_x, min_y=min_y, max_y=max_y)
+        print()
+
 def part1(data):
     grid, blizzards = parse_input(data)
-    location = Point(1, 2)
+    location = Point(2, 1)
 
     max_x = max((point.x for point in grid.keys()))
     min_x = min((point.x for point in grid.keys()))
@@ -74,7 +110,9 @@ def part1(data):
 
     goal = Point(max_x - 1, max_y)
 
-    states = [State.build(location, blizzards, goal, 0)]
+    replay(grid, blizzards, [location, (2,2)])
+
+    states = [State.build((location,), blizzards, goal, 0)]
     heapify(states)
 
     max_t = 0
@@ -100,12 +138,14 @@ def part1(data):
 
         if goal == state.location:
             best_t = state.time
+            print(state.path)
             print(f'found! {best_t}')
-            continue
+            break
+            # continue
 
         for next_location in moves(state.location):
             if next_location in grid and grid[next_location] != '#' and not in_blizzard(cache_blizzards(new_blizzards), next_location):
-                heappush(states, State.build(next_location, new_blizzards, goal, state.time + 1))
+                heappush(states, State.build(state.path + (next_location,), new_blizzards, goal, state.time + 1))
 
 
     return best_t
