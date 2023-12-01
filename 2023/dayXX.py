05cca503b243d4ca3f28ea8eae9994ed2952637c
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-11-30 23:49:56 -0500

    Add 2023 skeleton

---
+++ 2023/dayXX.py
@@ -0,0 +1,15 @@
+import fileinput
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+def main():
+    print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
+
+
+if __name__ == '__main__':
+    main()
