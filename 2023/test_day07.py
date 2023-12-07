a8bed243ed45f04208b3dc64782049ce454f4581
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-07 03:04:57 -0500

    Add input data tests

--- 2023/test_day07.py
+++ 2023/test_day07.py
@@ -9,9 +9,23 @@ def example_data():
         yield data
 
 
+@pytest.fixture
+def input_data():
+    with fileinput.input('day07.in') as data:
+        yield data
+
+
 def test_part1_example(example_data):
     assert part1(example_data) == 6440
 
 
 def test_part2_example(example_data):
     assert part2(example_data) == 5905
+
+
+def test_part1_input(input_data):
+    assert part1(input_data) == 251029473
+
+
+def test_part2_input(input_data):
+    assert part2(input_data) == 251003917
