11b6bccc0e12924e565c7598e8eb29c480982f23
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-07 01:35:10 -0500

    Refactor to combine score hand method

--- 2023/day07.py
+++ 2023/day07.py
@@ -34,19 +34,11 @@ class HandRank(enum.IntEnum):
     FOUR_OF_KIND = 6
     FIVE_OF_KIND = 7
 
-@dataclasses.dataclass
-class HandBid:
-    hand: list[CardRank]
-    bet: int
 
-    def __lt__(self, other):
-        return compareHands(self.hand, other.hand) == 1
+def parse_card(card, *, jokers=False):
+    if jokers and card == 'J':
+        return CardRank.JOKER
 
-    def __eq__(self, other):
-        return compareHands(self.hand, other.hand) == 0
-
-
-def parse_card(card):
     return {
         'A': CardRank.ACE,
         'K': CardRank.KING,
@@ -63,67 +55,29 @@ def parse_card(card):
         '2': CardRank.TWO
     }[card]
 
-def parse_card2(card):
-    return {
-        'A': CardRank.ACE,
-        'K': CardRank.KING,
-        'Q': CardRank.QUEEN,
-        'J': CardRank.JOKER,
-        'T': CardRank.TEN,
-        '9': CardRank.NINE,
-        '8': CardRank.EIGHT,
-        '7': CardRank.SEVEN,
-        '6': CardRank.SIX,
-        '5': CardRank.FIVE,
-        '4': CardRank.FOUR,
-        '3': CardRank.THREE,
-        '2': CardRank.TWO
-    }[card]
-
 
 def scoreHand(hand):
     c = collections.Counter(hand)
 
     best = c.most_common()
-    # print(f'{best=}')
-
-    if best[0][1] == 5:
-        return HandRank.FIVE_OF_KIND
-    elif best[0][1] == 4:
-        return HandRank.FOUR_OF_KIND
-    elif best[0][1] == 3:
-        if best[1][1] == 2:
-            return HandRank.FULL_HOUSE
-        return HandRank.THREE_OF_KIND
-    elif best[0][1] == 2:
-        if best[1][1] == 2:
-            return HandRank.TWO_PAIR
-        return HandRank.ONE_PAIR
-    else:
-        return HandRank.HIGH_CARD
-
-def scoreHand2(hand):
-    c = collections.Counter(hand)
-
-    best = c.most_common()
     # print(f'{hand=}: {best=} {c=}')
 
-    if best[0][0] == 'J':
+    if best[0][0] == CardRank.JOKER:
         offset = 1
     else:
         offset = 0
 
-    if c['J'] == 5:
+    if c[CardRank.JOKER] == 5:
         return HandRank.FIVE_OF_KIND
-    elif best[0+offset][1] + c['J'] == 5:
+    elif best[0+offset][1] + c[CardRank.JOKER] == 5:
         return HandRank.FIVE_OF_KIND
-    elif best[0+offset][1] + c['J'] == 4:
+    elif best[0+offset][1] + c[CardRank.JOKER] == 4:
         return HandRank.FOUR_OF_KIND
-    elif best[0+offset][1] + c['J'] == 3:
+    elif best[0+offset][1] + c[CardRank.JOKER] == 3:
         if best[1+offset][1] == 2:
             return HandRank.FULL_HOUSE
         return HandRank.THREE_OF_KIND
-    elif best[0+offset][1] + c['J'] == 2:
+    elif best[0+offset][1] + c[CardRank.JOKER] == 2:
         if best[1+offset][1] == 2:
             return HandRank.TWO_PAIR
         return HandRank.ONE_PAIR
@@ -139,8 +93,9 @@ def compareHands(hand1, hand2):
         return -1
     else:
         for c1, c2 in zip(hand1, hand2):
-            cc1 = parse_card(c1)
-            cc2 = parse_card(c2)
+            # cc1 = parse_card(c1)
+            # cc2 = parse_card(c2)
+            cc1, cc2 = c1, c2
             if cc1 > cc2:
                 return 1
             elif cc2 > cc1:
@@ -149,15 +104,16 @@ def compareHands(hand1, hand2):
             return 0
 
 def compareHands2(hand, other):
-    s1, s2 = scoreHand2(hand), scoreHand2(other)
+    s1, s2 = scoreHand(hand), scoreHand(other)
     if s1 > s2:
         return 1
     elif s2 > s1:
         return -1
     else:
         for c1, c2 in zip(hand, other):
-            cc1 = parse_card2(c1)
-            cc2 = parse_card2(c2)
+            # cc1 = parse_card2(c1)
+            # cc2 = parse_card2(c2)
+            cc1, cc2 = c1, c2
             if cc1 > cc2:
                 return 1
             elif cc2 > cc1:
@@ -173,34 +129,28 @@ def compare_bets2(bet1, bet2):
     return compareHands2(bet1[0], bet2[0])
 
 
-def parse_cards(cards):
-    return [parse_card(card) for card in cards]
-
-
-def parse_line(line):
+def parse_line(line, jokers=False):
     hand, bet = line.split(' ')
-    return parse_cards(hand), int(bet)
+    cards = [parse_card(card, jokers=jokers) for card in hand]
+    return cards, int(bet)
 
-def parse_data(lines):
-    return [parse_line(line) for line in lines]
+def parse_data(lines, jokers=False):
+    return [parse_line(line, jokers=jokers) for line in lines]
 
 
 def part1(data):
     acc = 0
-    for index, (_hand, bet) in enumerate(sorted((item.split(' ') for item in data), key=functools.cmp_to_key(compare_bets))):
-        acc += (index+1) * int(bet)
+    for index, (_, bet) in enumerate(sorted(parse_data(data), key=functools.cmp_to_key(compare_bets)), start=1):
+        acc += index * bet
 
     return acc
 
 def part2(data):
     acc = 0
-    for index, (_hand, bet) in enumerate(sorted((item.split(' ') for item in data), key=functools.cmp_to_key(compare_bets2))):
-        acc += (index+1) * int(bet)
+    for index, (_, bet) in enumerate(sorted(parse_data(data, True), key=functools.cmp_to_key(compare_bets2)), start = 1):
+        acc += index * bet
 
-    CardRank.FIVE
-    pass
     return acc
-    pass
 
 def main():
     print(part1(fileinput.input()))
