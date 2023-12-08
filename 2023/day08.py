2d2bbfcc6184c7106be7a970dd9229e6f576d948
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-08 00:35:53 -0500

    Optimize part2

--- 2023/day08.py
+++ 2023/day08.py
@@ -40,12 +40,20 @@ def part1(data):
     return step
     pass
 
+
+def lcm(*vals):
+    print(vals)
+    lcm = vals[0]
+    for next_val in vals[1:]:
+        lcm = lcm * next_val // math.gcd(lcm, next_val)
+
+    return lcm
+
 def part2(data):
     tree = {}
 
     instructions = list(next(data).strip())
     print(f'{instructions=}')
-    instructions_loop = itertools.cycle(instructions)
 
     next(data)
 
@@ -61,23 +69,27 @@ def part2(data):
 
     locations = starts
 
-    step = 0
-    while any(map(lambda item: not item.endswith('Z'), locations)):
-        instruction = next(instructions_loop)
-        code = 0 if instruction == 'L' else 1
-        # print(f'{instruction=} {location=}')
-        for i, location in enumerate(locations):
-            locations[i] = tree[location][code]
-        step += 1
+    steps = [0] * len(locations)
 
+    for i, location in enumerate(locations):
+        instructions_loop = itertools.cycle(instructions)
+        step = 0
 
-        if any(map(lambda item: item.endswith('Z'), locations)):
-            print(f'{step=}: {locations=}')
+        while not locations[i].endswith('Z'):
+            instruction = next(instructions_loop)
+            code = 0 if instruction == 'L' else 1
+            # print(f'{instruction=} {location=}')
+            locations[i] = tree[locations[i]][code]
+            step += 1
+
+        steps[i] = step
+
 
         # if step > 3:
             # break
+    print(f'{steps=}')
 
-    return step
+    return lcm(*steps)
     pass
 
 def main():
