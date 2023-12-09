e2a5c270be27f472a778182edf8c1d72d788c32e
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-09 00:46:16 -0500

    Add tests for day09

---
+++ 2023/test_day09.py
@@ -0,0 +1,17 @@
+import pytest
+import fileinput
+from day09 import part1, part2
+
+
+@pytest.fixture
+def example_data():
+    with fileinput.input('day09.example') as data:
+        yield data
+
+
+def test_part1_example(example_data):
+    assert part1(example_data) == 114
+
+
+def test_part2_example(example_data):
+    assert part2(example_data) == 2
