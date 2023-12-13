5111ba2c7bb5040eff6977c7285f6f037cdda8e8
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-13 01:12:54 -0500

    Refactor part 1 and 2 to use same logic

--- 2023/day13.py
+++ 2023/day13.py
@@ -1,10 +1,4 @@
 import fileinput
-import re
-import itertools
-import math
-import functools
-import collections
-import enum
 
 
 def transpose(grid):
@@ -18,71 +12,42 @@ def extract_patterns(data):
         yield chunk.strip().split('\n')
 
 
-def find_horizontal_reflection(pattern):
-    for index, _ in enumerate(pattern):
-        before = list(reversed(pattern[:index]))
-        after = pattern[index:]
-
-        if len(before) == 0 or len(after) == 0:
-            continue
-
-        if all(b == a for a, b in zip(before, after)):
-            return index
-    return 0
-
-
 def count_diff(a, b):
-    acc = 0
-    for aa, bb in zip(a, b):
-        if aa != bb:
-            acc += 1
+    return sum(1 for i, j in zip(a, b) if i != j)
 
-    return acc
 
-def find_smugged_horizontal_reflection(pattern):
-    for index, _ in enumerate(pattern):
-        before = list(reversed(pattern[:index]))
+def find_horizontal_reflection(pattern, smudges=0):
+    for index, _ in enumerate(pattern[1:], start=1):
+        before = reversed(pattern[:index])
         after = pattern[index:]
 
-        if len(before) == 0 or len(after) == 0:
-            continue
-
-        if sum(count_diff(a, b)for a, b in zip(before, after)) == 1:
+        if sum(count_diff(*pair) for pair in zip(before, after)) == smudges:
             return index
+
     return 0
 
 
-def find_vertical_reflection(pattern):
-    return find_horizontal_reflection(transpose(pattern))
+def find_vertical_reflection(pattern, smudges=0):
+    return find_horizontal_reflection(transpose(pattern), smudges)
 
-def find_smugged_vertical_reflection(pattern):
-    return find_smugged_horizontal_reflection(transpose(pattern))
+
+def find_reflection(pattern, smudges=0):
+    horizontal = find_horizontal_reflection(pattern, smudges)
+    vertical = find_vertical_reflection(pattern, smudges)
+
+    return horizontal * 100 + vertical
 
 
 def part1(data):
     patterns = list(extract_patterns(data))
 
-    acc = 0
-
-    for pattern in patterns:
-        if (row_index := find_horizontal_reflection(pattern)):
-            acc += row_index * 100
-        elif (col_index := find_vertical_reflection(pattern)):
-            acc += col_index
-    return acc
+    return sum(find_reflection(pattern, 0) for pattern in patterns)
 
 
 def part2(data):
     patterns = list(extract_patterns(data))
 
-    acc = 0
-
-    for pattern in patterns:
-        if (row_index := find_smugged_horizontal_reflection(pattern)):
-            acc += row_index * 100
-        elif (col_index := find_smugged_vertical_reflection(pattern)):
-            acc += col_index
-    return acc
+    return sum(find_reflection(pattern, 1) for pattern in patterns)
 
 
 def main():
