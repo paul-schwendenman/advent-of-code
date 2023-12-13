008142b1092eaf03e3f62c3e11a03cd82b0647c7
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-13 00:58:36 -0500

    Refactor solution to remove cruft

--- 2023/day13.py
+++ 2023/day13.py
@@ -19,31 +19,16 @@ def extract_patterns(data):
 
 
 def find_horizontal_reflection(pattern):
-    for index, line in enumerate(pattern):
+    for index, _ in enumerate(pattern):
         before = list(reversed(pattern[:index]))
         after = pattern[index:]
 
         if len(before) == 0 or len(after) == 0:
             continue
 
-        # print(f'{pattern=}')
-
-        # print(f'{before=} {after=}')
-
-        # print(f'{index:02}: {all(b == a for a, b in zip(before, after))} = {list(b == a for a, b in zip(before, after))}')
         if all(b == a for a, b in zip(before, after)):
             return index
-
-        # for b, a in zip(before, after):
-        #     print(f'{a=} {b=}')
-        #     if b == a:
-        #         pass
-        #     else:
-        #         continue
-        # else:
-        #     return index
-    else:
-        raise ValueError("No match found")
+    return 0
 
 
 def count_diff(a, b):
@@ -55,7 +40,7 @@ def count_diff(a, b):
     return acc
 
 def find_smugged_horizontal_reflection(pattern):
-    for index, line in enumerate(pattern):
+    for index, _ in enumerate(pattern):
         before = list(reversed(pattern[:index]))
         after = pattern[index:]
 
@@ -64,9 +49,7 @@ def find_smugged_horizontal_reflection(pattern):
 
         if sum(count_diff(a, b)for a, b in zip(before, after)) == 1:
             return index
-
-    else:
-        raise ValueError("No match found")
+    return 0
 
 
 def find_vertical_reflection(pattern):
@@ -79,61 +62,27 @@ def find_smugged_vertical_reflection(pattern):
 def part1(data):
     patterns = list(extract_patterns(data))
 
-    print(f'{len(patterns)}')
-
     acc = 0
 
     for pattern in patterns:
-        try:
-            n = find_horizontal_reflection(pattern) * 100
-            print(f'{n=}')
-            acc += n
-        except:
-            pass
-        finally:
-            pass
-        try:
-            m = find_vertical_reflection(pattern)
-            print(f'{m=}')
-            acc += m
-        except:
-            pass
-        finally:
-            pass
+        if (row_index := find_horizontal_reflection(pattern)):
+            acc += row_index * 100
+        elif (col_index := find_vertical_reflection(pattern)):
+            acc += col_index
     return acc
-
-
-
-
-    pass
 
 
 def part2(data):
     patterns = list(extract_patterns(data))
 
-    print(f'{len(patterns)}')
-
     acc = 0
 
     for pattern in patterns:
-        try:
-            n = find_smugged_horizontal_reflection(pattern) * 100
-            print(f'{n=}')
-            acc += n
-        except:
-            pass
-        finally:
-            pass
-        try:
-            m = find_smugged_vertical_reflection(pattern)
-            print(f'{m=}')
-            acc += m
-        except:
-            pass
-        finally:
-            pass
+        if (row_index := find_smugged_horizontal_reflection(pattern)):
+            acc += row_index * 100
+        elif (col_index := find_smugged_vertical_reflection(pattern)):
+            acc += col_index
     return acc
-    pass
 
 
 def main():
