6f26f87e80f3c2c39abbb935ffec4199c6c084d7
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-13 23:53:59 -0500

    Add script to initial days

--- 2023/new-day.sh
+++ 2023/new-day.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 
-cp dayXX.py day13.py
-touch day13.in day13.example
+if [ $# -ne 1 ]; then
+    echo "illegal number of parameters"
+fi
+
+if [ ! -f day$1.py ]; then
+    cp dayXX.py day$1.py
+    touch day$1.in day$1.example
+elif [ ! -f test_day$1.py ]; then
+    cp test_dayXX.py test_day$1.py
+fi
