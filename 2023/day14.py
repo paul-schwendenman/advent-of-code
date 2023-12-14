e08e74db73dfbbe34d517142230af6037ba8d6bd
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-14 01:20:32 -0500

    Refactor part1

--- 2023/day14.py
+++ 2023/day14.py
@@ -5,6 +5,7 @@ import math
 import functools
 import collections
 import enum
+from tqdm import tqdm
 
 
 class Point(collections.namedtuple('Point', 'x y')):
@@ -21,9 +22,7 @@ class Point(collections.namedtuple('Point', 'x y')):
 def transpose(grid):
     return list(zip(*grid))
 
-
-def part1(data):
-    offset = (0, -1)
+def parse_data(data):
     grid = collections.defaultdict(lambda: '#')
 
     for j, line in enumerate(data):
@@ -31,13 +30,11 @@ def part1(data):
             grid[Point(i, j)] = space
     else:
         max_x, max_y = i + 1, j + 1
-    pass
 
-    # for j in range(11):
-    #     print(''.join(grid.get((i, j), ' ') for i in range(11)))
+    return grid, (max_x, max_y)
 
-    # print(f'{max_x=} {max_y=}')
 
+def tilt(grid, max_x, max_y, shift):
     moved = True
     while moved:
         moved = False
@@ -45,18 +42,20 @@ def part1(data):
         for j in range(1, max_y):
             for i in range(0, max_x):
                 pos = Point(i, j)
+                next_pos = pos + shift
 
                 if grid[pos] in ('.', '#'):
                     continue
-                if grid[nxt := (i, j-1)] == '.':
+                if grid[next_pos] == '.':
                     moved = True
-                    grid[nxt] = 'O'
+                    grid[next_pos] = 'O'
                     grid[pos] = '.'
-                    pass
+    return grid
 
+def part1(data):
+    grid, (max_x, max_y) = parse_data(data)
 
-    # for j in range(11):
-    #     print(''.join(grid.get((i, j), ' ') for i in range(11)))
+    grid = tilt(grid, max_x, max_y, (0, -1))
 
     return sum(max_y - y for (_, y), space in grid.items() if space == 'O' )
 
@@ -70,12 +69,41 @@ def part1(data):
 
 
 def part2(data):
+    offsets = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+    grid = collections.defaultdict(lambda: '#')
+
+    for j, line in enumerate(data):
+        for i, space in enumerate(line.strip()):
+            grid[Point(i, j)] = space
+    else:
+        max_x, max_y = i + 1, j + 1
+    pass
+
+    for cycle in tqdm(itertools.count(1_000), total=1000):
+        for offset in offsets:
+            moved = True
+            while moved:
+                moved = False
+
+                for j in range(1, max_y):
+                    for i in range(0, max_x):
+                        pos = Point(i, j)
+
+                        if grid[pos] in ('.', '#'):
+                            continue
+                        if grid[nxt := ((i, j) + offset)] == '.':
+                            moved = True
+                            grid[nxt] = 'O'
+                            grid[pos] = '.'
+                            pass
+
+    return sum(max_y - y for (_, y), space in grid.items() if space == 'O' )
     pass
 
 
 def main():
     print(part1(fileinput.input()))
-    print(part2(fileinput.input()))
+    # print(part2(fileinput.input()))
 
 
 if __name__ == '__main__':
