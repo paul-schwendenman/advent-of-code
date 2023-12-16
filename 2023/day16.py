078c1cc4f830fb7e38495deae2abbfb3e3b546e4
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-16 01:19:51 -0500

    Clean day 16

--- 2023/day16.py
+++ 2023/day16.py
@@ -11,11 +11,6 @@ import pprint
 class Point(collections.namedtuple('Point', 'x y')):
     __slots__ = ()
 
-    def get_neighbors(self):
-        # for offset in ((-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (-1, 1), (-1, -1), (1, -1)):
-        for offset in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            yield self + offset
-
     def __add__(self, other):
         return Point(self.x + other[0], self.y + other[1])
 
@@ -64,7 +59,6 @@ def track_beams(grid, start_location, start_direction):
     queue = collections.deque([(start_location, start_direction)])
 
     while len(queue) > 0:
-        # print(f'{energized=}')
         location, facing = queue.popleft()
 
         if (location, facing) in visited:
@@ -77,7 +71,6 @@ def track_beams(grid, start_location, start_direction):
 
         match grid.get(location, None), facing:
             case None, _:
-                # print('hit border', location, facing)
                 continue
             case '.', _:
                 next_location = location + facing
@@ -136,17 +129,14 @@ def track_beams(grid, start_location, start_direction):
                 queue.append((next_location, facing))
             case symbol, direction:
                 raise ValueError("No match", symbol, direction)
-        # show_energized(grid, energized)
     return energized
 
 
 def part1(data):
     grid, _, _ = parse_grid(data)
 
-    start = Point(3, 0)
-    start_direction = Direction.DOWN
-    # start = Point(0, 0)
-    # start_direction = Direction.RIGHT
+    start = Point(0, 0)
+    start_direction = Direction.RIGHT
 
     energized = track_beams(grid, start, start_direction)
 
@@ -162,24 +152,12 @@ def part2(data):
     top_row = ((Point(x, 0), Direction.DOWN) for x in range(max_x + 1))
     bottom_row = ((Point(x, max_y), Direction.UP) for x in range(max_x + 1))
 
-    # for start_location, start_direction in itertools.chain(top_row):
     for start_location, start_direction in itertools.chain(top_row, bottom_row, left_row, right_row):
-        # print(f'{start_location=} {start_direction=}')
         energized = track_beams(grid, start_location, start_direction)
 
         energized_map[(start_location, start_direction)] = energized
 
-    # pprint.pprint({key: len(value) for key, value in energized_map.items()})
-    # pprint.pprint({key: (value) for key, value in energized_map.items()})
-
     return max(len(a) for a in energized_map.values())
-    # return max((len(a | b), a, b) for a, b in itertools.combinations(energized_map.values(), 2))
-    top = max((len(a[1] | b[1]), a, b) for a, b in itertools.combinations(energized_map.items(), 2))
-
-    print(top[1][0], top[2][0],)
-    show_energized(grid, top[1][1] | top[2][1])
-
-    return len(top[1][1] | top[2][1])
 
 
 def main():
