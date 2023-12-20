7ce3c9cca3ac9fab023b9df928eda3e4438cdda6
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-12-20 15:34:22 -0500

    Add day 20 part 2

--- 2023/day20.py
+++ 2023/day20.py
@@ -140,6 +140,109 @@ def part1(data):
 
 
 def part2(data):
+    tree = {}
+    for line in data:
+        match = re.match(r'^(broadcaster) -> ((?:[a-z]+, )*[a-z]+)\n$', line)
+
+        if match:
+            name, children = match.groups()
+            nodes = [child.strip() for child in children.split(',')]
+
+            tree[name] = ModuleType.BROADCASTER, nodes
+            continue
+
+        match = re.match(r'^%([a-z]+) -> ((?:[a-z]+, )*[a-z]+)$', line)
+
+        if match:
+            name, children = match.groups()
+            nodes = [child.strip() for child in children.split(',')]
+            tree[name] = ModuleType.FLIPFLOP, nodes
+            continue
+
+        match = re.match(r'^&([a-z]+) -> ((?:[a-z]+, )*[a-z]+)$', line)
+
+        if match:
+            name, children = match.groups()
+            nodes = [child.strip() for child in children.split(',')]
+            tree[name] = ModuleType.CONJUNCTION, nodes
+            continue
+
+        raise ValueError("No match", line)
+
+
+    high, low = 0, 0
+
+    modules_memory = collections.defaultdict(lambda: Pulse.LOW)
+    modules_input = collections.defaultdict(dict)
+
+    for source, destinations in tree.items():
+        for destination in destinations[1]:
+            modules_input[destination][source] = Pulse.LOW
+
+    watch = ['cl', 'rp', 'lb', 'nj']
+
+    prev = {}
+    seen = collections.defaultdict(int)
+    multiples = []
+
+    for button_press in itertools.count(1):
+        queue = collections.deque([State('broadcaster', 'button', Pulse.LOW)])
+
+
+        while len(queue) > 0:
+
+            node = queue.popleft()
+            name, source, pulse = node
+
+            if pulse == Pulse.LOW:
+                if name in prev and name in watch and seen[name] == 2:
+                    multiples.append(button_press - prev[name])
+                    pass
+                prev[name] = button_press
+                seen[name] += 1
+            if len(multiples) == len(watch):
+                return math.lcm(*multiples)
+
+            if name == 'rx' and pulse == Pulse.LOW:
+                return button_press
+
+            if pulse == Pulse.HIGH:
+                high += 1
+            else:
+                low += 1
+
+            module_type, nxt_nodes = tree.get(name, (None, []))
+
+            if module_type is None:
+                continue
+
+            if module_type == ModuleType.BROADCASTER:
+                next_pulse = pulse
+
+            elif module_type == ModuleType.FLIPFLOP:
+                if pulse == Pulse.LOW:
+                    if modules_memory[name] == Pulse.HIGH:
+                        modules_memory[name] = Pulse.LOW
+                    else:
+                        modules_memory[name] = Pulse.HIGH
+
+                    next_pulse = modules_memory[name]
+
+                else:
+                    continue
+
+            elif module_type == ModuleType.CONJUNCTION:
+                modules_input[name][source] = pulse
+
+                if all(pulse == Pulse.HIGH for pulse in modules_input[name].values()):
+                    next_pulse = Pulse.LOW
+                else:
+                    next_pulse = Pulse.HIGH
+
+            for next_node in nxt_nodes:
+                queue.append(State(next_node, name, next_pulse))
+
+    return high * low
     pass
 
 
