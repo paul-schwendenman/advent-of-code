1dc2ee03f7c0387e26cbea7905e86c75793bdcf7
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-11-30 23:07:13 -0500

    Add day01 files

---
+++ 2024/day01.py
@@ -0,0 +1,26 @@
+import fileinput
+import re
+import itertools
+import math
+import functools
+import collections
+import enum
+import pprint
+import typing
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
+
+
+if __name__ == '__main__':
+    main()
