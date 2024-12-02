0eccb8d5816f2d5dad76a01b9492e5046f48b13a
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-02 00:46:52 -0500

    Add part2

--- 2024/day02.py
+++ 2024/day02.py
@@ -9,22 +9,25 @@ import pprint
 import typing
 
 
+def check_report(levels):
+    diffs = [b - a for a, b in (zip(levels[:-1], levels[1:]))]
+
+
+    all_neg = all(diff < 0 for diff in diffs)
+    all_pos = all(diff > 0 for diff in diffs)
+
+    all_small = all(abs(diff) in (1, 2, 3) for diff in diffs)
+
+    return all_small and (all_neg or all_pos)
+
+
 def part1(data):
     count = 0
     for line in data:
         safe = True
         levels = [int(item) for item in line.strip().split()]
-        # print(levels)
 
-        diffs = [b - a for a, b in (zip(levels[:-1], levels[1:]))]
-        # print(diffs)
-
-        all_neg = all(diff < 0 for diff in diffs)
-        all_pos = all(diff > 0 for diff in diffs)
-        all_small = all(abs(diff) in (1, 2, 3) for diff in diffs)
-        # print(all_neg, all_pos, all_small)
-
-        if all_small and (all_neg or all_pos):
+        if check_report(levels):
             # print("pass")
             count += 1
 
@@ -34,7 +37,20 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    count = 0
+    for line in data:
+        levels = [int(item) for item in line.strip().split()]
+
+        if check_report(levels):
+            count += 1
+        else:
+            for i in range(len(levels)):
+                new_levels = levels[:i] + levels[i+1:]
+                if check_report(new_levels):
+                    count += 1
+                    break
+
+    return count
 
 
 def main():
