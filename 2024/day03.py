50478d90d15923666ec87db8d2e26799e857be71
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-03 01:24:33 -0500

    Refactor to extract instruction totaling

--- 2024/day03.py
+++ 2024/day03.py
@@ -9,35 +9,30 @@ import pprint
 import typing
 
 
-def part1(data):
-    lines = [line for line in data]
-    count = 0
-
-    for line in lines:
-        matches = re.findall(r'mul\(\d+,\d+\)', line)
-
-        for match in matches:
-            nums = list(map(int, re.findall(r'\d+', match)))
-
-            count += math.prod(nums)
-
-    return count
-
-
-def part2(data):
-    line = ''.join(line.strip() for line in data)
-    count = 0
-
-    line = re.sub(r'don\'t\(\).*?do\(\)', '', line)
-
+def total_instructions(line):
     matches = re.findall(r'mul\(\d+,\d+\)', line)
+    total = 0
 
     for match in matches:
         nums = list(map(int, re.findall(r'\d+', match)))
 
-        count += math.prod(nums)
+        total += math.prod(nums)
 
-    return count
+    return total
+
+
+def part1(data):
+    lines = [line for line in data]
+
+    return sum(total_instructions(line) for line in lines)
+
+
+def part2(data):
+    single_line = ''.join(line.rstrip() for line in data)
+
+    enabled_instructions = re.sub(r'don\'t\(\).*?do\(\)', '', single_line)
+
+    return total_instructions(enabled_instructions)
 
 
 def main():
