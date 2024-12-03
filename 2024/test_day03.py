88132692c75a03d833d76f4dcee3af41b0485134
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-03 00:38:10 -0500

    Add tests

---
+++ 2024/test_day03.py
@@ -0,0 +1,30 @@
+import pytest
+import fileinput
+from day03 import part1, part2
+
+
+@pytest.fixture
+def example_data():
+    with fileinput.input('day03.example') as data:
+        yield data
+
+@pytest.fixture
+def input_data():
+    with fileinput.input('day3.in') as data:
+        yield data
+
+
+def test_part1_example(example_data):
+    assert part1(example_data) == 161
+
+
+def test_part2_example(example_data):
+    assert part2(example_data) == 48
+
+
+def test_part1_input(input_data):
+    assert part1(input_data) == 174336360
+
+
+def test_part2_input(input_data):
+    assert part2(input_data) == 88802350
