dfc02626bbf3a36226b66d193aaff62f79986ffc
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-03 00:40:43 -0500

    Fix example part2 test to use correct file

--- 2024/test_day03.py
+++ 2024/test_day03.py
@@ -8,6 +8,13 @@ def example_data():
     with fileinput.input('day03.example') as data:
         yield data
 
+
+@pytest.fixture
+def example2_data():
+    with fileinput.input('day03.example2') as data:
+        yield data
+
+
 @pytest.fixture
 def input_data():
     with fileinput.input('day3.in') as data:
@@ -18,8 +25,8 @@ def test_part1_example(example_data):
     assert part1(example_data) == 161
 
 
-def test_part2_example(example_data):
-    assert part2(example_data) == 48
+def test_part2_example(example_data2):
+    assert part2(example_data2) == 48
 
 
 def test_part1_input(input_data):
