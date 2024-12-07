cc4df05efede3cf018f5fc1160af870d1a98efdc
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-07 13:48:37 -0500

    Remove more dead code

--- 2024/day07.py
+++ 2024/day07.py
@@ -27,7 +27,6 @@ def get_possible_values(parts, acc):
 
 def is_possible(goal, parts):
     [first, *rest] = parts
-    # print(list(flatten(get_possible_values(rest, first))))
     return goal in get_possible_values(rest, first)
 
 
@@ -38,7 +37,6 @@ def part1(data):
         equations.append((goal, collections.deque(parts)))
 
     acc = 0
-    # print(equations)
 
     for equation in equations:
         if is_possible(*equation):
@@ -64,7 +62,6 @@ def get_possible_values2(parts, acc):
 def is_possible2(goal, parts):
     [first, *rest] = parts
     values = list(get_possible_values2(rest, first))
-    # print(f'{goal}\t{goal in values}\t', values)
     return goal in values
 
 
@@ -73,7 +70,7 @@ def part2(data):
     equations = []
     for line in data:
         [goal, *parts] = extract_ints(line)
-        equations.append((goal, collections.deque(parts)))
+        equations.append((goal, parts))
 
     acc = 0
 
@@ -81,9 +78,7 @@ def part2(data):
         if is_possible2(*equation):
             acc += equation[0]
 
-    pass
     return acc
-    pass
 
 
 def main():
