ff359860f56349af3488daaa250c99cf921f4aac
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-09 01:26:17 -0500

    Add part2

--- 2024/day09.py
+++ 2024/day09.py
@@ -87,7 +87,66 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    line = [line for line in data][0].strip()
+
+    index = 0
+    file_no = 0
+    disc = {}
+    files = {}
+    spaces = {}
+
+    for char, is_file in zip(line, itertools.cycle([True, False])):
+        size = int(char)
+
+        if is_file:
+            files[file_no] = (index, size)
+        else:
+            spaces[index] = size
+
+        for _ in range(size):
+            if is_file:
+                disc[index] = file_no
+            index += 1
+
+        if is_file:
+            file_no += 1
+
+    # print(files)
+    # print(spaces)
+
+
+    left = 0
+    right = index
+
+    # print('\t', end='')
+    # print_disc(disc, index)
+    while right not in disc:
+        right -= 1
+
+    for file_no, details in reversed(files.items()):
+        # print(f'file: {file_no}\t', end="")
+        location, size = details
+
+        for new_spot in range(location):
+            empty_size = spaces.get(new_spot, 0)
+            if empty_size >= size:
+                del spaces[new_spot]
+                if empty_size > size:
+                    spaces[new_spot+size] = empty_size - size
+                for new_location in range(new_spot, new_spot+size):
+                    disc[new_location] = file_no
+                for old_location in range(location, location+size):
+                    del disc[old_location]
+
+                break
+
+        # print_disc(disc, index)
+
+    acc = 0
+    for i in range(index):
+        acc += i * disc.get(i, 0)
+
+    return acc
 
 
 def main():
