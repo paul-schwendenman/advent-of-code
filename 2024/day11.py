7f11bd087e302e66d4020900bd20b013187ab198
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-11 00:31:08 -0500

    Add part 2

--- 2024/day11.py
+++ 2024/day11.py
@@ -7,6 +7,7 @@ import collections
 import enum
 import pprint
 import typing
+from tqdm import tqdm
 
 def extract_ints(string):
     return list(map(int, re.findall(r'-?\d+', string)))
@@ -17,11 +18,11 @@ def part1(data):
     nums = extract_ints(line)
     new_nums = []
 
-    print(f'nums: {nums}')
+    # print(f'nums: {nums}')
 
 
-    for _ in range(25):
-        # print('-------------')
+    for index in tqdm(range(25)):
+        print(f'------ {index} - {len(nums)} -------')
         for n in nums:
             if n == 0:
                 new_nums.append(1)
@@ -44,7 +45,45 @@ def part1(data):
     pass
 
 
+@functools.lru_cache
+def blink_stone(stone):
+    if stone == 0:
+        return [1]
+    elif (size := len(str(stone))) % 2 == 0:
+        half = size // 2
+        front = int(str(stone)[:half])
+        back = int(str(stone)[half:])
+
+        return [front, back]
+    else:
+        return [stone * 2024]
+
+
+def blink_stones(stone_counts):
+    counts = collections.Counter()
+
+    for stone, count in stone_counts.items():
+        for stone in blink_stone(stone):
+            counts[stone] += count
+
+    return counts
+
+
+
 def part2(data):
+    line = [l for l in data][0]
+    nums = extract_ints(line)
+    new_nums = []
+
+    stone_counts = collections.Counter(nums)
+
+    # print(f'nums: {nums}')
+
+
+    for index in tqdm(range(75)):
+        stone_counts = blink_stones(stone_counts)
+
+    return sum(count for count in stone_counts.values())
     pass
 
 
