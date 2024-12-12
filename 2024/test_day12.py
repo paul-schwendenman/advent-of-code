07f8db4194397e72f1863c77cc7d15626517f009
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-12 01:46:24 -0500

    Add tests used for checking part1

---
+++ 2024/test_day12.py
@@ -0,0 +1,38 @@
+import pytest
+import fileinput
+from day12 import part1, part2
+
+
+@pytest.fixture
+def example_data():
+    with fileinput.input('day12.example') as data:
+        yield data
+
+
+@pytest.fixture
+def example_data2():
+    with fileinput.input('day12.example2') as data:
+        yield data
+
+
+@pytest.fixture
+def example_data3():
+    with fileinput.input('day12.example3') as data:
+        yield data
+
+
+def test_part1_example(example_data):
+    assert part1(example_data) == 1930
+
+
+def test_part1_example2(example_data2):
+    assert part1(example_data2) == 140
+
+
+def test_part1_example3(example_data3):
+    assert part1(example_data3) == 772
+
+
+@pytest.mark.skip
+def test_part2_example(example_data):
+    assert part2(example_data) == None
