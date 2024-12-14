6f73004e63e4fb67c24751d08ac1451d2ae91c4d
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-14 02:33:45 -0500

    Add better handing of const values

--- 2024/day14.py
+++ 2024/day14.py
@@ -9,8 +9,14 @@ import pprint
 import typing
 from utils import *
 
+MAX_X, MAX_Y = 101, 103
+
 class Robot(collections.namedtuple('Robot', 'x y dx dy')):
     __slots__ = ()
+
+    def move(self, max_x=MAX_X, max_y=MAX_Y):
+        return Robot((self.x + self.dx) % max_x, (self.y + self.dy) % max_y, self.dx, self.dy)
+
 
 
 def print_grid(robots: list[Robot], max_x: int, max_y: int):
@@ -37,11 +43,11 @@ def parse_robots(data) -> list[Robot]:
     return [parse_robot(line) for line in data]
 
 
-def move_robots(robots: list[Robot], max_x: int, max_y: int) -> list[Robot]:
-    return [Robot((x + dx) % max_x, (y + dy) % max_y, dx, dy) for (x, y, dx, dy) in robots]
+def move_robots(robots: list[Robot], *, max_x: int = MAX_X, max_y: int = MAX_Y) -> list[Robot]:
+    return [robot.move(max_x, max_y) for robot in robots]
 
 
-def count_quadrants(robots: list[Robot], max_x: int, max_y: int) -> tuple[int, int, int, int]:
+def count_quadrants(robots: list[Robot], *, max_x: int = MAX_X, max_y: int = MAX_Y) -> tuple[int, int, int, int]:
     q1 = len([1 for robot in robots if robot[0] < max_x // 2 and robot[1] < max_y // 2])
     q2 = len([1 for robot in robots if robot[0] > max_x // 2 and robot[1] < max_y // 2])
     q3 = len([1 for robot in robots if robot[0] < max_x // 2 and robot[1] > max_y // 2])
@@ -50,24 +56,24 @@ def count_quadrants(robots: list[Robot], max_x: int, max_y: int) -> tuple[int, i
     return (q1, q2, q3, q4)
 
 
-def part1(data, max_x=101, max_y=103):
+def part1(data, max_x=MAX_X, max_y=MAX_Y):
     robots = parse_robots(data)
 
     for _ in range(100):
-        robots = move_robots(robots, max_x, max_y)
+        robots = move_robots(robots, max_x=max_x, max_y=max_y)
 
     quadrants = count_quadrants(robots=robots, max_x=max_x, max_y=max_y)
 
     return math.prod(quadrants)
 
 
-def part2(data, max_x=101, max_y=103):
+def part2(data):
     robots = parse_robots(data)
 
     num_robots = len(robots)
 
     for step in itertools.count(1):
-        robots = move_robots(robots, max_x, max_y)
+        robots = move_robots(robots)
 
         if num_robots == len(set((x, y) for x, y, _, _ in robots)):
             break
