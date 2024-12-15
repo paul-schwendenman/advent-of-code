1881334dc3cf82ab135293f7c2a69116acabb5d6
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-15 01:38:51 -0500

    Refactor move and can_move functions

--- 2024/day15.py
+++ 2024/day15.py
@@ -10,36 +10,10 @@ import typing
 from utils import *
 
 
-def can_move(grid, robot, offset):
-    next_space = robot + offset
-
-    if grid[next_space] == '.':
-        return True
-    elif grid[next_space] == '#':
-        return False
-
-    return can_move(grid, robot + offset, offset)
-
-
-def move(grid, robot, offset):
-    if grid[robot + offset] == '#':
-        raise ValueError('Can\'t move there')
-
-    if grid[robot + offset] == '.':
-        grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
-        return grid, robot + offset
-
-    grid, _ = move(grid, robot + offset, offset)
-
-    assert grid[robot + offset] == '.'
-    grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
-
-    return grid, robot + offset
-
-
 def print_grid(grid, max_x, max_y):
     for j in range(0, max_y+1):
         print("".join(grid.get((i, j), '.') for i in range(0, max_x+1)))
+
 
 def double_grid(grid):
     new_grid: Dict[Point, str] = {}
@@ -65,51 +39,53 @@ def double_grid(grid):
 
     return new_grid, robot
 
-def can_move2(grid, robot, offset):
-    next_space = robot + offset
 
-    if grid[next_space] == '.':
+def can_move(grid, location, offset):
+    next_location = location + offset
+
+    if grid[next_location] == '.':
         return True
-    elif grid[next_space] == '#':
+    elif grid[next_location] == '#':
         return False
-    elif grid[next_space] == '[' and offset in (Offset.UP, Offset.DOWN):
-        return can_move2(grid, next_space, offset) and can_move2(grid, next_space + (1, 0), offset)
-    elif grid[next_space] == '[' and offset in (Offset.LEFT, Offset.RIGHT):
-        return can_move2(grid, next_space, offset)
-    elif grid[next_space] == ']' and offset in (Offset.UP, Offset.DOWN):
-        return can_move2(grid, next_space, offset) and can_move2(grid, next_space + (-1, 0), offset)
-    elif grid[next_space] == ']' and offset in (Offset.LEFT, Offset.RIGHT):
-        return can_move2(grid, next_space, offset)
+    elif grid[next_location] == 'O':
+        return can_move(grid, next_location, offset)
+    elif grid[next_location] == '[' and offset in (Offset.UP, Offset.DOWN):
+        return can_move(grid, next_location, offset) and can_move(grid, next_location + Offset.RIGHT, offset)
+    elif grid[next_location] == '[' and offset in (Offset.LEFT, Offset.RIGHT):
+        return can_move(grid, next_location, offset)
+    elif grid[next_location] == ']' and offset in (Offset.UP, Offset.DOWN):
+        return can_move(grid, next_location, offset) and can_move(grid, next_location + Offset.LEFT, offset)
+    elif grid[next_location] == ']' and offset in (Offset.LEFT, Offset.RIGHT):
+        return can_move(grid, next_location, offset)
 
     raise ValueError()
 
 
-def move2(grid, robot, offset):
-    if grid[robot + offset] == '#':
-        raise ValueError('Can\'t move there')
+def move(grid, location, offset):
+    next_location = location + offset
+    if grid[next_location] == '#':
+        raise ValueError("Can't move there")
 
-    if grid[robot + offset] == '.':
-        grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
-        return grid, robot + offset
-    elif grid[robot + offset] == '[' and offset in (Offset.UP, Offset.DOWN):
-        grid, _ = move2(grid, robot + offset, offset)
-        grid, _ = move2(grid, robot + offset + (1, 0), offset)
-        grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
+    if grid[next_location] == '.':
+        grid[next_location], grid[location] = grid[location], grid[next_location]
+        return grid, next_location
+    elif grid[next_location] == '[' and offset in (Offset.UP, Offset.DOWN):
+        grid, _ = move(grid, next_location, offset)
+        grid, _ = move(grid, next_location + Offset.RIGHT, offset)
+        grid[next_location], grid[location] = grid[location], grid[next_location]
 
-        return grid, robot + offset
-    elif grid[robot + offset] == ']' and offset in (Offset.UP, Offset.DOWN):
-        grid, _ = move2(grid, robot + offset, offset)
-        grid, _ = move2(grid, robot + offset + (-1, 0), offset)
-        grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
+        return grid, next_location
+    elif grid[next_location] == ']' and offset in (Offset.UP, Offset.DOWN):
+        grid, _ = move(grid, next_location, offset)
+        grid, _ = move(grid, next_location + Offset.LEFT, offset)
+        grid[next_location], grid[location] = grid[location], grid[next_location]
 
-        return grid, robot + offset
+        return grid, next_location
 
-    grid, _ = move2(grid, robot + offset, offset)
+    grid, _ = move(grid, next_location, offset)
+    grid[next_location], grid[location] = grid[location], grid[next_location]
 
-    assert grid[robot + offset] == '.'
-    grid[robot + offset], grid[robot] = grid[robot], grid[robot + offset]
-
-    return grid, robot + offset
+    return grid, next_location
 
 def part1(data):
     parts = ''.join(line for line in data).split('\n\n')
@@ -117,12 +93,7 @@ def part1(data):
     grid, j, k, markers = parse_grid(parts[0].split('\n'))
     instructions = parts[1].rstrip()
 
-    # print(f'instructions: {instructions}')
     robot = markers['@'][0]
-
-    # print('Initial state:')
-    # print_grid(grid, j, k)
-    # print('')
 
     for instruction in instructions:
         if instruction == '<':
@@ -141,13 +112,7 @@ def part1(data):
         if can_move(grid, robot, offset):
             grid, robot = move(grid, robot, offset)
 
-        # print(f'Move {instruction}:')
-        # print_grid(grid, j, k)
-        # print('')
-
     return sum(100 * y + x for (x, y), value in grid.items() if value=='O')
-
-    pass
 
 
 def part2(data):
@@ -156,13 +121,8 @@ def part2(data):
     half_grid, j, k, _ = parse_grid(parts[0].split('\n'))
     instructions = parts[1].rstrip()
 
-    # print(f'instructions: {instructions}')
     grid, robot = double_grid(half_grid)
     j = j * 2 + 1
-
-    # print('Initial state:')
-    # print_grid(grid, j, k)
-    # print('')
 
     for instruction in instructions:
         if instruction == '<':
@@ -178,16 +138,10 @@ def part2(data):
         else:
             raise ValueError(f'Unknown instruction: "{instruction}"')
 
-        # print(f'Move {instruction}:')
-
-        if can_move2(grid, robot, offset):
-            grid, robot = move2(grid, robot, offset)
-
-        # print_grid(grid, j, k)
-        # print('')
+        if can_move(grid, robot, offset):
+            grid, robot = move(grid, robot, offset)
 
     return sum(100 * y + x for (x, y), value in grid.items() if value=='[')
-    pass
 
 
 def main():
