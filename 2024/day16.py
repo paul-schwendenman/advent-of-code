887b2c6d4ad6abd51dd61d8c42263d07a83d6aa0
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-16 02:45:27 -0500

    Add part2, very slow

--- 2024/day16.py
+++ 2024/day16.py
@@ -78,15 +78,69 @@ def part1(data):
 
 
 
-    pass
+def solve(grid, start, end):
+    facing = Offset.EAST
+    states = [(0, start, facing, [(start, facing)])]
+    heapify(states)
+
+    found = collections.defaultdict(lambda: math.inf)
+    points = collections.defaultdict(set)
+
+    # print_grid(grid, j, k, path=(start,))
+
+    while states:
+        state = heappop(states)
+        # print(f'{state}')
+
+        cost, location, facing, path = state
+        # print_grid(grid, j, k, path)
+        # input()
+
+        if found[location] < cost:
+            continue
+        found[location] = cost
+        points[location] |= set(i[0] for i in path)
+
+        if grid[location] == '#':
+            continue
+
+        heappush(states, (cost + 1, location + facing, facing, path + [(location + facing, facing)]))
+
+        for next_facing in (facing.rotate(False), facing.rotate(True)):
+            heappush(states, (cost + 1001, location + next_facing, next_facing, path + [(location + next_facing, next_facing)]))
+
+    return found[end], points[end]
 
 
 def part2(data):
+    grid, j, k, markers = parse_grid(data)
+
+    start = markers['S'][0]
+    end = markers['E'][0]
+
+    score, path = solve(grid, start, end)
+
+    for spot in list(path):
+        if spot in (start, end):
+            continue
+        grid[spot] = '#'
+
+        cost, p = solve(grid, start, end)
+
+        if cost == score:
+                path |= p
+
+        grid[spot] = '.'
+
+
+
+
+    return score, len(path)
     pass
 
 
 def main():
-    print(part1(fileinput.input()))
+    # print(part1(fileinput.input()))
     print(part2(fileinput.input()))
 
 
