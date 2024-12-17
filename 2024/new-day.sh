51d5defa6e47f74c882f3a874f15227eb0b39be4
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-16 23:56:16 -0500

    Add line to add new files

--- 2024/new-day.sh
+++ 2024/new-day.sh
@@ -9,6 +9,7 @@ if [ ! -f day$1.py ]; then
     cp dayXX.py day$1.py
     touch day$1.in day$1.example
     code day$1.py day$1.in day$1.example
+    git add -N day$1.py day$1.in day$1.example
 elif [ ! -f test_day$1.py ]; then
     cp test_dayXX.py test_day$1.py
     code test_day$1.py
