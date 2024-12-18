54e5a62e07d239ec6c586d2fac052ed3a53657f2
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-18 00:39:47 -0500

    Fix part 1

--- 2024/day18.py
+++ 2024/day18.py
@@ -7,6 +7,7 @@ import collections
 import enum
 import pprint
 import typing
+from heapq import heapify, heappop, heappush
 from utils import *
 
 
@@ -15,31 +16,34 @@ def part1(data):
 
     corrupted = set()
     start = Point(0, 0)
-    # goal, limit, max_x, max_y = Point(70, 70), 1024, 70, 70
-    goal, limit, max_x, max_y = Point(6, 6), 12, 6, 6
+    goal, limit, max_x, max_y = Point(70, 70), 1024, 70, 70
+    # goal, limit, max_x, max_y = Point(6, 6), 12, 6, 6
 
     for space in memory[:limit]:
         corrupted.add(space)
 
     found = {}
-    queue = collections.deque([(0, start)])
+    # queue = collections.deque([(0, start)])
+    queue = [(goal.manhattan(start), 0, start)]
 
     while queue:
-        state = queue.popleft()
+        # state = queue.popleft()
+        state = heappop(queue)
 
-        distance, location = state
+        _, distance, location = state
 
         if found.get(location, math.inf) < distance:
             continue
 
-        if location in corrupted:
-            continue
-
         found[location] = distance
 
+        if location == goal:
+            break
+
         for neighbor in location.get_neighbors(offsets=Offset.cardinal()):
-            if 0 <= neighbor.x <= max_x and 0 <= neighbor.y <= max_y:
-                queue.append((distance + 1, neighbor))
+            if 0 <= neighbor.x <= max_x and 0 <= neighbor.y <= max_y and neighbor not in corrupted:
+                # queue.append((distance + 1, neighbor))
+                heappush(queue, (goal.manhattan(neighbor), (distance + 1), neighbor))
 
     return found[goal]
 
