c601ec0f1b2b6e315001ddce1c742812afa38591
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-22 01:20:37 -0500

    Refactor part1 day 22

--- 2024/day22.py
+++ 2024/day22.py
@@ -9,66 +9,46 @@ import pprint
 import typing
 from utils import *
 
-def calc_next(secret):
-    secret = (secret ^ (secret << 6)) % 16777216
-    secret = (secret ^ (secret >> 5)) % 16777216
-    secret = (secret ^ (secret << 11 )) % 16777216
+
+def calc_next(secret, n=1):
+    for _ in range(n):
+        secret = (secret ^ (secret << 6)) % 16777216    # Multiply by 64, 64 = 2 ** 6
+        secret = (secret ^ (secret >> 5)) % 16777216    # Divide by 32, 32 = 2 ** 5
+        secret = (secret ^ (secret << 11 )) % 16777216  # Multiply by 2024, 2024 = 2 ** 11
+
     return secret
 
 
 def part1(data):
-    acc = 0
-    for line in data:
-        secret = int(line)
-        new_secret = secret
-        for _ in range(2000):
-            new_secret = calc_next(new_secret)
+    secrets = [int(line) for line in data]
 
-        acc += new_secret
-        print(f'{secret}: {new_secret}')
-
-    return acc
-
-
-    pass
+    return sum(calc_next(secret, 2000) for secret in secrets)
 
 
 def part2(data):
-    acc = 0
+    secrets = [int(line) for line in data]
     changes_map = defaultdict(dict)
 
-    for line in data:
+    for secret in secrets:
         changes = collections.deque(maxlen=4)
-        secret = int(line)
         price = secret % 10
         new_secret = secret
-        # print(f'{secret:10d}: {price}')
+
         for _ in range(2000):
-        # for _ in range(10 - 1):
             new_secret = calc_next(new_secret)
             new_price = new_secret % 10
 
             price_diff = new_price - price
             changes.append(price_diff)
 
-            # print(f'{new_secret:10d}: {new_price} ({price_diff}) {tuple(changes)}')
-
             if len(changes) == 4 and secret not in changes_map[tuple(changes)]:
                 changes_map[tuple(changes)][secret] = new_price
-            pass
 
             price = new_price
 
-        acc += new_secret
-        # print(f'{secret}: {new_secret}')
-
-    all_diffs = [sum(value.values()) for key, value in changes_map.items()]
-    # pprint.pprint(all_diffs)
-    # pprint.pprint(changes_map)
-    pprint.pprint(changes_map[(-2,1,-1,3)])
+    all_diffs = [sum(value.values()) for value in changes_map.values()]
 
     return max(all_diffs)
-    pass
 
 
 def main():
