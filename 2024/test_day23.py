754cd72c2ac0334bdeec949a4b4589cbb08828a9
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-23 01:07:00 -0500

    Add tests to benchmark performance

--- 2024/test_day23.py
+++ 2024/test_day23.py
@@ -15,3 +15,17 @@ def test_part1_example(example_data):
 
 def test_part2_example(example_data):
     assert part2(example_data) == 'co,de,ka,ta'
+
+
+def test_part1_speed(benchmark, example_data):
+    lines = [line for line in example_data]
+    value = benchmark(part1, lines)
+
+    assert value == 7
+
+
+def test_part2_speed(benchmark, example_data):
+    lines = [line for line in example_data]
+    value = benchmark(part2, lines)
+
+    assert value == 'co,de,ka,ta'
