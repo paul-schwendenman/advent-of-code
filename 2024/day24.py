389f6cdad36a83d8f258453a79c35c201302cd9d
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-24 00:55:59 -0500

    Add some investigation

--- 2024/day24.py
+++ 2024/day24.py
@@ -10,6 +10,28 @@ import typing
 from utils import *
 
 
+def get_int(gates, prefix='z'):
+    number = [1 if gates.get(f'{prefix}{index:02d}', False) else 0 for index in range(99)]
+    value = 0
+
+    for digit in reversed(number):
+        value = value << 1
+        value += digit
+
+    return value
+
+
+def follow_path(gates: dict[str, tuple[str, str, str]], goal: str):
+    if goal.startswith('x'):
+        return goal
+    elif goal.startswith('y'):
+        return goal
+
+    op, a, b = gates[goal]
+
+    return f'({follow_path(gates, a)}) {op} ({follow_path(gates, b)})'
+
+
 def part1(data):
     initials, instructions = [chunk.strip().split('\n') for chunk in ''.join(data).split('\n\n')]
     pass
@@ -20,8 +42,8 @@ def part1(data):
         name, value = i.split(': ')
         gates[name] = value == '1'
 
-    print(gates)
     queue = collections.deque(instructions)
+    instructions_map = {}
 
     while queue:
         instruction = queue.popleft()
@@ -32,6 +54,8 @@ def part1(data):
         if a not in gates or b not in gates:
             queue.append(instruction)
             continue
+
+        instructions_map[c] = (op, a, b)
 
         if op == 'AND':
             gates[c] = gates[a] & gates[b]
@@ -45,6 +69,23 @@ def part1(data):
         else:
             raise ValueError(f'Invalid Op: {op}')
 
+    x = (get_int(gates, 'x'))
+    y = (get_int(gates, 'y'))
+    z = (get_int(gates, 'z'))
+
+    print(f'{x:46b} = {x}')
+    print(f'{y:46b} = {y}')
+    print('-' * 50)
+    print(f'{x+y:46b} = {x+y} = {x} + {y}')
+    print(f'{z:b} = {z}')
+
+    print(f'{(x+y) ^ z:46b}')
+
+    # for i in range(46):
+    #     z = f'z{i:02d}'
+    #     print(f'{z} = {follow_path(instructions_map, z)}')
+
+    return z
     z = [1 if gates.get(f'z{index:02d}', False) else 0 for index in range(99)]
     print(z)
 
@@ -55,15 +96,6 @@ def part1(data):
         value += digit
 
     return value
-
-
-    return
-
-
-
-
-
-
 
 
 def part2(data):
