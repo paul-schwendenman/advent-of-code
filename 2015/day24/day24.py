52fa95ea150a914536efdcee12fe0141fcc8054b
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-31 00:37:32 -0500

    Add part2

--- 2015/day24/day24.py
+++ 2015/day24/day24.py
@@ -6,28 +6,25 @@ import functools
 import collections
 import enum
 import pprint
+import operator
 import typing
+from functools import reduce
+from operator import mul
 
 
-def part1(data):
+def part1(data, number_of_groups=3):
     weights = [int(line) for line in data]
 
-    goal_weight = sum(weights) // 3
+    goal_weight = sum(weights) // number_of_groups
 
     for group_size in range(len(weights)):
         for group in itertools.combinations(weights, group_size):
             if sum(group) == goal_weight:
-                return functools.reduce(lambda a, b: math.prod([a, b]), group)
-
-
-
-
-
-    pass
+                return reduce(mul, group)
 
 
 def part2(data):
-    pass
+    return part1(data, 4)
 
 
 def main():
