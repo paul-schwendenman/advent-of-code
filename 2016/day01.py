524e789cfadfd2e248b9d3ebd1e000a5c96bb09c
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-02 18:41:48 -0500

    Add part2

--- 2016/day01.py
+++ 2016/day01.py
@@ -35,17 +35,59 @@ def walk_path(instructions):
     return abs(x) + abs(y)
 
 
+def walk_path2(instructions):
+    facing = {
+        1: "N",
+        -1: "S",
+        (-1j): "W",
+        (1j): "E",
+    }
+    x, y, f = 0, 0, 1
+
+    seen = set()
+
+    for instruction in instructions:
+        instruction = instruction.strip()
+        turn, length = instruction[0], int(instruction[1:])
+
+        if turn == 'L':
+            f *= -1j
+        elif turn == 'R':
+            f *= 1j
+        else:
+            raise ValueError(instruction)
+
+        for i in range(length):
+            if f == 1:
+                x += 1 #length
+            elif f == -1:
+                x -= 1 #length
+            elif f == 1j:
+                y += 1 #length
+            elif f == -1j:
+                y -= 1 #length
+            else:
+                ValueError(f'facing: {f}')
+
+            # if (x, y) in seen:
+            #     break
+            seen.add((x, y))
+
+    print(f'{x=}, {y=}, {f=}, {facing[f]}')
+    return abs(x) + abs(y)
+
+
 def part1(data):
     return walk_path(item.strip() for item in next(data).split(','))
 
 
 def part2(data):
-    pass
+    return walk_path2(item.strip() for item in next(data).split(','))
 
 
 def main():
-    print(part1(fileinput.input()))
-    # print(part2(fileinput.input()))
+    # print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
 
 
 if __name__ == '__main__':
