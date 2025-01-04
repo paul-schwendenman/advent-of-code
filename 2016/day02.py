4ece144496b39cf0819bc2581054e2c2f33bc6d7
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-03 21:42:09 -0500

    Add part1

--- 2016/day02.py
+++ 2016/day02.py
@@ -10,8 +10,41 @@ import typing
 from utils import *
 
 
+keypad = {
+    Point(0, 0): '1',
+    Point(1, 0): '2',
+    Point(2, 0): '3',
+    Point(0, 1): '4',
+    Point(1, 1): '5',
+    Point(2, 1): '6',
+    Point(0, 2): '7',
+    Point(1, 2): '8',
+    Point(2, 2): '9',
+}
+
+
+directions = {
+    'U': Offset.UP,
+    'D': Offset.DOWN,
+    'L': Offset.LEFT,
+    'R': Offset.RIGHT,
+}
+
+
 def part1(data):
-    pass
+    nums = ""
+    location = Point(1,1)
+
+    for line in data:
+        for char in line.rstrip():
+            next_location = location + directions[char]
+
+            if next_location in keypad:
+                location = next_location
+
+        nums += keypad[location]
+
+    return int(nums)
 
 
 def part2(data):
