856359a76658824f8b3bc8b353ef23044e72b934
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-03 23:35:46 -0500

    Add part 1

--- 2016/day04.py
+++ 2016/day04.py
@@ -10,8 +10,31 @@ import typing
 from utils import *
 
 
+class Room(collections.namedtuple('Room', 'encrypted_name sector_id checksum valid')):
+    __slots__ = ()
+
+
+def calc_checksum(encrypted_name):
+    c = collections.Counter(encrypted_name)
+    del c['-']
+    pairs = c.most_common(5)
+
+    return ''.join(pair[0] for pair in pairs)
+
+
+def check_room(line):
+    match = re.match(r'([a-z-]+)-([0-9]+)\[([a-z]+)\]', line)
+    encrypted_name, sector_id, checksum = match.groups()
+
+    valid = checksum == calc_checksum(encrypted_name)
+
+    return Room(encrypted_name, int(sector_id), checksum, valid)
+
+
 def part1(data):
-    pass
+    rooms = [check_room(line) for line in data]
+
+    return sum(room.sector_id for room in rooms if room.valid)
 
 
 def part2(data):
