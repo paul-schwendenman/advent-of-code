5ef9e13e16de27dc79a0efdace8e0f1120a799dd
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-06 01:05:40 -0500

    Add regex solution

--- 2016/day07.py
+++ 2016/day07.py
@@ -11,11 +11,30 @@ from utils import *
 
 
 def check_ip(ip):
+    print(f'--- {ip} ----')
+    match = re.match(r'.*(\w)(\w)\2\1', ip)
+
+    if not match:
+        print(f'skipping: {ip}')
+        return False
+
+    print(f'group: {match.groups()}')
+
+    match2 = re.match(r'.*\[(\w)(\w)\2\1\]', ip)
+
+    if match2:
+        return False
+    groups = match.group()
+
+    if groups[0] == groups[1]:
+        return False
+
+    print(match, match2)
     return True
 
 
 def part1(data):
-    return sum(1 for line in data if check_ip(line))
+    return sum(1 for line in data if check_ip(line.rstrip()))
 
 
 def part2(data):
