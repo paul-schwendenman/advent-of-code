6a6dfece751b17f05370f2f28847c7ecb23b8511
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-06 00:46:55 -0500

    Add skeleton

---
+++ 2016/day07.py
@@ -0,0 +1,27 @@
+import fileinput
+import re
+import itertools
+import math
+import functools
+import collections
+import enum
+import pprint
+import typing
+from utils import *
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    print(part1(fileinput.input()))
+    print(part2(fileinput.input()))
+
+
+if __name__ == '__main__':
+    main()
