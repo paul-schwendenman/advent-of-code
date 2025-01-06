c17cbedd48c9e93be0df6e9fbe21a05eeed1fefb
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-06 00:15:15 -0500

    Add part2

--- 2016/day05.py
+++ 2016/day05.py
@@ -27,7 +27,26 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    door_id = [line.rstrip() for line in data][0]
+
+    password = {}
+    index = 0
+
+    while len(password.keys()) < 8:
+        while not (hashed := hashlib.md5((door_id + str(index)).encode('utf-8')).hexdigest()).startswith('00000'):
+            index += 1
+        position = int(hashed[5], base=16)
+        value = hashed[6]
+
+        if 0 <= position < 8:
+            password[position] = value
+            print('\r' + ''.join(password.get(i, ' ') for i in range(8)), end='')
+
+        index += 1
+
+    print('\r')
+
+    return ''.join(password[i] for i in range(8))
 
 
 def main():
