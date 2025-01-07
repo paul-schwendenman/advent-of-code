f7b6eedc1d4c276f18254a26422ac8d289892d26
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-06 21:48:59 -0500

    Add part2
    gross

--- 2016/day07.py
+++ 2016/day07.py
@@ -17,13 +17,42 @@ def check_ip(ip):
 
     return len(matches) > 0
 
+def check_ssl(ip):
+    hyper = re.findall(r'\[\w+\]', ip)
+
+    for set in hyper:
+        ip = ip.replace(set, '')
+
+    chunks = [ip[i-2:i+1] for i in range(2, len(ip)) if ip[i-2] == ip[i] and ip[i] != ip[i-1]]
+
+    print(hyper)
+
+    print(chunks)
+
+    for chunk in chunks:
+        hcukn = chunk[1] + chunk[0] + chunk[1]
+
+        # if chunk[0] == chunk[1]:
+        #     continue
+
+        for h in hyper:
+            # print(f'{hcukn} {chunk} \t {ip}')
+            if hcukn in h:
+                # print(f'{hcukn} {chunk} \t {ip}')
+                return True
+    else:
+        print(f'no matches: {ip}')
+
+    return False
+
+
 
 def part1(data):
     return sum(1 for line in data if check_ip(line.rstrip()))
 
 
 def part2(data):
-    pass
+    return sum(1 for line in data if check_ssl(line.rstrip()))
 
 
 def main():
