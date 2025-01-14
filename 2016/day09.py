70caab9b96483de9fb7458987e9906ec54c5de24
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-01-14 00:32:13 -0500

    Add part2

--- 2016/day09.py
+++ 2016/day09.py
@@ -25,12 +25,27 @@ def decompress(file):
     return file[:position] + file[start:end] * repeat + decompress(file[end:])
 
 
+def decompress2(file):
+    bracket = re.search(r'\((\d+)x(\d+)\)', file)
+
+    if not bracket:
+        return file
+
+    position = bracket.start(0)
+    length = int(bracket.group(1))
+    repeat = int(bracket.group(2))
+    start = position + len(bracket.group())
+    end = start + length
+
+    return file[:position] + decompress2(file[start:end]) * repeat + decompress2(file[end:])
+
+
 def part1(data):
     return len(decompress(''.join(line.rstrip() for line in data)))
 
 
 def part2(data):
-    pass
+    return len(decompress2(''.join(line.rstrip() for line in data)))
 
 
 def main():
